a344aba0a1c3ba594e6aaecedd2da7dd65d17f39
Schombert <pschombe+b@gmail.com>
2023-01-28 01:04:59 -0800

    Partway through getting text to work

--- src/text/fonts.cpp
+++ src/text/fonts.cpp
@@ -97,4 +97,285 @@ uint32_t font_index_from_font_id(uint16_t id) {
 	return uint32_t((id >> 9) & 0xFF);
 }
 
+font_manager::font_manager() {
+	FT_Init_FreeType(&ft_library);
 }
+font_manager::~font_manager() {
+	FT_Done_FreeType(ft_library);
+}
+
+font::~font() {
+	if(loaded)
+		FT_Done_Face(font_face);
+}
+
+
+int32_t transform_offset_b(int32_t x, int32_t y, int32_t btmap_x_off, int32_t btmap_y_off, uint32_t width, uint32_t height, uint32_t pitch) {
+	int bmp_x = x - btmap_x_off;
+	int bmp_y = y - btmap_y_off;
+
+	if((bmp_x < 0) | (bmp_x >= (int32_t)width) | (bmp_y < 0) | (bmp_y >= (int32_t)height))
+		return -1;
+	else
+		return bmp_x + bmp_y * (int32_t)pitch;
+}
+
+
+constexpr int magnification_factor = 4;
+constexpr int dr_size = 64 * magnification_factor;
+constexpr float rt_2 = 1.41421356237309504f;
+
+void init_in_map(bool in_map[dr_size * dr_size], uint8_t* bmp_data, int32_t btmap_x_off, int32_t btmap_y_off, uint32_t width, uint32_t height, uint32_t pitch) {
+	for(int32_t j = 0; j < dr_size; ++j) {
+		for(int32_t i = 0; i < dr_size; ++i) {
+			const auto boff = transform_offset_b(i, j, btmap_x_off, btmap_y_off, width, height, pitch);
+			in_map[i + dr_size * j] = (boff != -1) ? (bmp_data[boff] > 127) : false;
+		}
+	}
+}
+
+//
+// based on The "dead reckoning" signed distance transform
+// Grevera, George J. (2004) Computer Vision and Image Understanding 95 pages 317–333
+//
+
+void dead_reckoning(float distance_map[dr_size * dr_size], const bool in_map[dr_size * dr_size]) {
+	int16_t yborder[dr_size * dr_size] = { 0 };
+	int16_t xborder[dr_size * dr_size] = { 0 };
+
+	for(uint32_t i = 0; i < dr_size * dr_size; ++i) {
+		distance_map[i] = std::numeric_limits<float>::infinity();
+	}
+	for(int32_t j = 1; j < dr_size - 1; ++j) {
+		for(int32_t i = 1; i < dr_size - 1; ++i) {
+			if(in_map[i - 1 + dr_size * j] != in_map[i + dr_size * j] ||
+				in_map[i + 1 + dr_size * j] != in_map[i + dr_size * j] ||
+				in_map[i + dr_size * (j + 1)] != in_map[i + dr_size * j] ||
+				in_map[i + dr_size * (j - 1)] != in_map[i + dr_size * j]) {
+				distance_map[i + dr_size * j] = 0.0f;
+				yborder[i + dr_size * j] = static_cast<int16_t>(j);
+				xborder[i + dr_size * j] = static_cast<int16_t>(i);
+			}
+		}
+	}
+	for(int32_t j = 1; j < dr_size - 1; ++j) {
+		for(int32_t i = 1; i < dr_size - 1; ++i) {
+			if(distance_map[(i - 1) + dr_size * (j - 1)] + rt_2 < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i - 1) + dr_size * (j - 1)];
+				xborder[i + dr_size * j] = xborder[(i - 1) + dr_size * (j - 1)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+			if(distance_map[(i)+dr_size * (j - 1)] + 1.0f < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i)+dr_size * (j - 1)];
+				xborder[i + dr_size * j] = xborder[(i)+dr_size * (j - 1)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+			if(distance_map[(i + 1) + dr_size * (j - 1)] + rt_2 < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i + 1) + dr_size * (j - 1)];
+				xborder[i + dr_size * j] = xborder[(i + 1) + dr_size * (j - 1)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+			if(distance_map[(i - 1) + dr_size * (j)] + 1.0f < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i - 1) + dr_size * (j)];
+				xborder[i + dr_size * j] = xborder[(i - 1) + dr_size * (j)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+		}
+	}
+
+	for(int32_t j = dr_size - 2; j > 0; --j) {
+		for(int32_t i = dr_size - 2; i > 0; --i) {
+			if(distance_map[(i + 1) + dr_size * (j)] + 1.0f < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i + 1) + dr_size * (j)];
+				xborder[i + dr_size * j] = xborder[(i + 1) + dr_size * (j)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+			if(distance_map[(i - 1) + dr_size * (j + 1)] + rt_2 < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i - 1) + dr_size * (j + 1)];
+				xborder[i + dr_size * j] = xborder[(i - 1) + dr_size * (j + 1)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+			if(distance_map[(i)+dr_size * (j + 1)] + 1.0f < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i)+dr_size * (j + 1)];
+				xborder[i + dr_size * j] = xborder[(i)+dr_size * (j + 1)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+			if(distance_map[(i + 1) + dr_size * (j + 1)] + rt_2 < distance_map[(i)+dr_size * (j)]) {
+				yborder[i + dr_size * j] = yborder[(i + 1) + dr_size * (j + 1)];
+				xborder[i + dr_size * j] = xborder[(i + 1) + dr_size * (j + 1)];
+				distance_map[(i)+dr_size * (j)] =
+					(float)std::sqrt((i - xborder[i + dr_size * j]) * (i - xborder[i + dr_size * j]) +
+					(j - yborder[i + dr_size * j]) * (j - yborder[i + dr_size * j]));
+			}
+		}
+	}
+
+	for(uint32_t i = 0; i < dr_size * dr_size; ++i) {
+		if(in_map[i])
+			distance_map[i] *= -1.0f;
+	}
+}
+
+void font_manager::load_font(font& fnt, char* file_data, uint32_t file_size) {
+	fnt.file_data = std::unique_ptr<FT_Byte[]>(new FT_Byte[file_size]);
+	memcpy(fnt.file_data.get(), file_data, file_size);
+	FT_New_Memory_Face(ft_library, fnt.file_data.get(), file_size, 0, &fnt.font_face);
+	FT_Select_Charmap(fnt.font_face, FT_ENCODING_UNICODE);
+	FT_Set_Pixel_Sizes(fnt.font_face, 0, 64 * magnification_factor);
+	fnt.loaded = true;
+
+	fnt.internal_line_height = static_cast<float>(fnt.font_face->size->metrics.height) / static_cast<float>((1 << 6) * magnification_factor);
+	fnt.internal_ascender = static_cast<float>(fnt.font_face->size->metrics.ascender) / static_cast<float>((1 << 6) * magnification_factor);
+	fnt.internal_descender = static_cast<float>(fnt.font_face->size->metrics.descender) / static_cast<float>((1 << 6) * magnification_factor);
+	fnt.internal_top_adj = (fnt.internal_line_height - (fnt.internal_ascender + fnt.internal_descender)) / 2.0f;
+
+	// load all glyph metrics
+
+	for(int32_t i = 0; i < 256; ++i) {
+		const auto index_in_this_font = FT_Get_Char_Index(fnt.font_face, win1250toUTF16(char(i)));
+		if(index_in_this_font) {
+			FT_Load_Glyph(fnt.font_face, index_in_this_font, FT_LOAD_TARGET_NORMAL);
+			fnt.glyph_advances[i] = static_cast<float>(fnt.font_face->glyph->metrics.horiAdvance) / static_cast<float>((1 << 6) * magnification_factor);
+		}
+	}
+}
+
+float font::kerning(char codepoint_first, char codepoint_second) const {
+	auto utf16_first = win1250toUTF16(codepoint_first);
+	auto utf16_second = win1250toUTF16(codepoint_second);
+	const auto index_a = FT_Get_Char_Index(font_face, utf16_first);
+	const auto index_b = FT_Get_Char_Index(font_face, utf16_second);
+
+	if((index_a == 0) || (index_b == 0)) {
+		return 0.0f;
+	}
+
+	if(FT_HAS_KERNING(font_face)) {
+		FT_Vector kerning;
+		FT_Get_Kerning(font_face, index_a, index_b, FT_KERNING_DEFAULT, &kerning);
+		return static_cast<float>(kerning.x) / static_cast<float>((1 << 6) * magnification_factor);
+	} else {
+		return 0.0f;
+	}
+}
+
+float font::line_height(int32_t size) const {
+	return internal_line_height * size / 64.0f;
+}
+float font::ascender(int32_t size) const {
+	return internal_ascender * size / 64.0f;
+}
+float font::descender(int32_t size) const {
+	return internal_descender * size / 64.0f;
+}
+float font::top_adjustment(int32_t size) const {
+	return internal_top_adj * size / 64.0f;
+}
+
+
+void font::make_glyph(char ch_in) {
+	if(glyph_loaded[ch_in])
+		return;
+	glyph_loaded[ch_in] = true;
+
+	auto codepoint = win1250toUTF16(ch_in);
+	if(codepoint == ' ')
+		return;
+
+	const auto index_in_this_font = FT_Get_Char_Index(font_face, codepoint);
+	if(index_in_this_font) {
+		FT_Load_Glyph(font_face, index_in_this_font, FT_LOAD_TARGET_NORMAL | FT_LOAD_RENDER);
+
+		FT_Glyph g_result;
+		FT_Get_Glyph(font_face->glyph, &g_result);
+
+		auto texture_number = uint32_t(ch_in) >> 6;
+
+		if(textures[texture_number] == 0) {
+			glGenTextures(1, &textures[texture_number]);
+			glBindTexture(GL_TEXTURE_2D, textures[texture_number]);
+			glTexStorage2D(GL_TEXTURE_2D, 1, GL_R8, 64 * 8, 64 * 8);
+
+			//glClearTexImage(textures[texture_number], 0, GL_RED, GL_FLOAT, nullptr);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		} else {
+			glBindTexture(GL_TEXTURE_2D, textures[texture_number]);
+		}
+
+		FT_Bitmap& bitmap = ((FT_BitmapGlyphRec*)g_result)->bitmap;
+
+		const float hb_x = static_cast<float>(font_face->glyph->metrics.horiBearingX) / static_cast<float>(1 << 6);
+		const float hb_y = static_cast<float>(font_face->glyph->metrics.horiBearingY) / static_cast<float>(1 << 6);
+
+		auto sub_index = (uint32_t(ch_in) & 63);
+
+		uint8_t pixel_buffer[64 * 64];
+
+		const int btmap_x_off = 32 * magnification_factor - bitmap.width / 2;
+		const int btmap_y_off = 32 * magnification_factor - bitmap.rows / 2;
+
+		glyph_positions[ch_in].x = (hb_x - static_cast<float>(btmap_x_off)) * 1.0f / static_cast<float>(magnification_factor);
+		glyph_positions[ch_in].y = (-hb_y - static_cast<float>(btmap_y_off)) * 1.0f / static_cast<float>(magnification_factor);
+
+		bool in_map[dr_size * dr_size] = { false };
+		float distance_map[dr_size * dr_size];
+
+		init_in_map(in_map, bitmap.buffer, btmap_x_off, btmap_y_off, bitmap.width, bitmap.rows, (uint32_t)bitmap.pitch);
+		dead_reckoning(distance_map, in_map);
+
+
+		for(int y = 0; y < 64; ++y) {
+			for(int x = 0; x < 64; ++x) {
+
+				const size_t index = static_cast<size_t>(x + y * 64);
+				const float distance_value = distance_map[
+					(x * magnification_factor + magnification_factor / 2) +
+						(y * magnification_factor + magnification_factor / 2) * dr_size]
+					/ static_cast<float>(magnification_factor * 64);
+					const int int_value = static_cast<int>(distance_value * -255.0f + 128.0f);
+					const uint8_t small_value = static_cast<uint8_t>(std::min(255, std::max(0, int_value)));
+
+					pixel_buffer[index] = small_value;
+			}
+		}
+
+		glTexSubImage2D(GL_TEXTURE_2D, 0,
+			(sub_index & 7) * 64,
+			((sub_index >> 3) & 7) * 64,
+			64,
+			64,
+			GL_RED, GL_UNSIGNED_BYTE, pixel_buffer);
+
+		FT_Done_Glyph(g_result);
+	}
+}
+
+float font::text_extent(const char* codepoints, uint32_t count, int32_t size) const {
+	float total = 0.0f;
+	for(; count-- > 0; ) {
+
+		total +=
+			this->glyph_advances[codepoints[count]] * size / 64.0f +
+			((count != 0) ? kerning(codepoints[count - 1], codepoints[count]) * size / 64.0f : 0.0f);
+	}
+	return total;
+}
+
+}
+
--- src/text/fonts.hpp
+++ src/text/fonts.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "ft2build.h"
+#include "freetype/freetype.h"
+#include "freetype/ftglyph.h"
+#include "unordered_dense.h"
 
 namespace text {
 
@@ -7,5 +11,52 @@ uint16_t name_into_font_id(std::string_view text);
 int32_t size_from_font_id(uint16_t id);
 bool is_black_from_font_id(uint16_t id);
 uint32_t font_index_from_font_id(uint16_t id);
+
+struct glyph_sub_offset {
+	float x = 0.0f;
+	float y = 0.0f;
+};
+
+class font {
+public:
+	FT_Face font_face;
+	float internal_line_height = 0.0f;
+	float internal_ascender = 0.0f;
+	float internal_descender = 0.0f;
+	float internal_top_adj = 0.0f;
+
+	bool loaded = false;
+
+	float glyph_advances[256] = { 0.0f };
+	uint32_t textures[4] = { 0, 0, 0, 0 };
+	bool glyph_loaded[256] = { false };
+	glyph_sub_offset glyph_positions[256] = {};
+
+	std::unique_ptr<FT_Byte[]> file_data;
+
+	~font();
+
+	void make_glyph(char ch_in);
+	float line_height(int32_t size) const;
+	float ascender(int32_t size) const;
+	float descender(int32_t size) const;
+	float top_adjustment(int32_t size) const;
+	float kerning(char codepoint_first, char codepoint_second) const;
+	float text_extent(const char* codepoints, uint32_t count, int32_t size) const;
+};
+
+class font_manager {
+public:
+	font_manager();
+	~font_manager();
+
+	font fonts[2];
+
+	FT_Library ft_library;
+
+	void load_font(font& fnt, char* file_data, uint32_t file_size);
+};
+
+void load_standard_fonts(sys::state& state);
 
 }
