c8f8f2f0a63eac92d627510d43c3ec65ecc0ffd3
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-02-05 13:47:08 +0800

    First draft GUI

--- src/gui/gui_diplomacy_window.hpp
+++ src/gui/gui_diplomacy_window.hpp
@@ -4,18 +4,18 @@
 
 namespace ui {
 
-class production_window_close_button : public button_element_base {
+class diplomacy_window_close_button : public button_element_base {
 public:
 	void button_action(sys::state& state) noexcept override {
 		state.ui_state.tab_window->set_visible(state, false);
 	}
 };
 
-class production_window : public window_element_base {
+class diplomacy_window : public window_element_base {
 public:
 	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
 		if(name == "close_button") {
-			return make_element_by_type<production_window_close_button>(state, id);
+			return make_element_by_type<diplomacy_window_close_button>(state, id);
 		} else {
 			return nullptr;
 		}
--- src/gui/gui_topbar.hpp
+++ src/gui/gui_topbar.hpp
@@ -2,6 +2,13 @@
 
 #include "gui_element_types.hpp"
 #include "gui_production_window.hpp"
+#include "gui_diplomacy_window.hpp"
+#include "gui_technology_window.hpp"
+#include "gui_politics_window.hpp"
+#include "gui_budget_window.hpp"
+#include "gui_trade_window.hpp"
+#include "gui_population_window.hpp"
+#include "gui_military_window.hpp"
 
 namespace ui {
 
@@ -22,35 +29,41 @@ public:
 	}
 };
 
-class topbar_production_button : public button_element_base {
+class topbar_tab_button : public button_element_base {
 public:
 	void button_action(sys::state& state) noexcept override {
-		if(!state.ui_state.production_window) {
-			auto new_pw = make_element_by_type<production_window>(state, "country_production");
-			state.ui_state.production_window = new_pw.get();
-			state.ui_state.root->add_child_to_front(std::move(new_pw));
-		} else if(state.ui_state.production_window->is_visible()) {
-			state.ui_state.production_window->set_visible(state, false);
-			state.ui_state.root->move_child_to_back(state.ui_state.production_window);
+		auto override_and_show_tab = [&]() {
+			state.ui_state.tab_background_image = tab_background_image;
+			state.ui_state.tab_background_image->set_visible(state, true);
+			state.ui_state.root->move_child_to_back(state.ui_state.tab_background_image);
+			state.ui_state.tab_window = tab_window;
+			state.ui_state.tab_window->set_visible(state, true);
+			state.ui_state.root->move_child_to_back(state.ui_state.tab_window);
+		};
+		if(state.ui_state.tab_window->is_visible() || state.ui_state.tab_background_image->is_visible()) {
+			state.ui_state.tab_window->set_visible(state, false);
+			state.ui_state.tab_background_image->set_visible(state, false);
+			if(state.ui_state.tab_window != tab_window || state.ui_state.tab_background_image != tab_background_image) {
+				override_and_show_tab();
+			}
 		} else {
-			state.ui_state.production_window->set_visible(state, true);
-			state.ui_state.root->move_child_to_front(state.ui_state.production_window);
+			override_and_show_tab();
 		}
 	}
 
 	void render(sys::state& state, int32_t x, int32_t y) noexcept override {
-		frame = int32_t(state.ui_state.production_window && state.ui_state.production_window->is_visible());
+		frame = int32_t(state.ui_state.tab_window && state.ui_state.tab_window->is_visible());
 		button_element_base::render(state, x, y);
 	}
+
+	element_base* tab_window = nullptr;
+	element_base* tab_background_image = nullptr;
 };
 
 class topbar_window : public window_element_base {
 public:
 	void on_create(sys::state& state) noexcept override {
 		window_element_base::on_create(state);
-		auto tc_prod = make_element_by_type<background_image>(state, "topcolor_production");
-		production_topcolor = tc_prod.get();
-		add_child_to_back(std::move(tc_prod));
 		auto bg_pic = make_element_by_type<background_image>(state, "bg_main_menus");
 		background_pic = bg_pic.get();
 		add_child_to_back(std::move(bg_pic));
@@ -58,25 +71,126 @@ public:
 	
 	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
 		if(name == "topbarbutton_production") {
-			return make_element_by_type<topbar_production_button>(state, id);
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_production");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			state.ui_state.tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<production_window>(state, "country_production");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.tab_window = tab.get(); // Default to this tab... but keep it hidden!
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_budget") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_budget");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<budget_window>(state, "country_budget");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_technology") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_technology");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<technology_window>(state, "country_technology");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_politics") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_politics");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<politics_window>(state, "country_politics");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_population") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_pop");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<population_window>(state, "country_pop");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_trade") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_trade");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<trade_window>(state, "country_trade");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_diplomacy") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_diplomacy");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<diplomacy_window>(state, "country_diplomacy");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
+		} else if(name == "topbarbutton_military") {
+			auto pw = make_element_by_type<topbar_tab_button>(state, id);
+
+			auto topcolor = make_element_by_type<background_image>(state, "topcolor_military");
+			topcolor->set_visible(state, false);
+			pw->tab_background_image = topcolor.get();
+			add_child_to_back(std::move(topcolor));
+
+			auto tab = make_element_by_type<military_window>(state, "country_military");
+			tab->set_visible(state, false);
+			pw->tab_window = tab.get();
+			state.ui_state.root->add_child_to_back(std::move(tab));
+			return pw;
 		} else {
 			return nullptr;
 		}
 	}
 
 	void render(sys::state& state, int32_t x, int32_t y) noexcept override {
-		if(state.ui_state.production_window && state.ui_state.production_window->is_visible()) {
-			production_topcolor->set_visible(state, true);
+		if(state.ui_state.tab_window && state.ui_state.tab_window->is_visible()) {
 			background_pic->set_visible(state, true);
 		} else {
-			production_topcolor->set_visible(state, false);
 			background_pic->set_visible(state, false);
 		}
 		window_element_base::render(state, x, y);
 	}
 
 private:
-	element_base* production_topcolor;
 	element_base* background_pic;
 };
 
