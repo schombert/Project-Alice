4d4e982da33cbc669f643b4c00a89ff89149e0a0
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-02-08 10:30:49 +0800

    textbox -> edit_box

--- src/gui/gui_console.hpp
+++ src/gui/gui_console.hpp
@@ -4,9 +4,9 @@
 
 namespace ui {
 
-class console_edit : public textbox_element_base {
+class console_edit : public edit_box_element_base {
 public:
-	virtual void textbox_enter(sys::state& state, std::string_view s) noexcept;
+	virtual void edit_box_enter(sys::state& state, std::string_view s) noexcept;
 };
 
 class console_list : public listbox_element_base {
