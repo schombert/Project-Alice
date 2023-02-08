8bbff430224b89c20ab8f119a46db53ed1f8a395
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-02-08 09:57:06 +0800

    Textbox controls

--- src/gui/gui_element_types.hpp
+++ src/gui/gui_element_types.hpp
@@ -127,9 +127,17 @@ public:
 };
 
 class textbox_element_base : public simple_text_element_base {
+	int32_t edit_index = 0;
 public:
-	message_result on_lbutton_down(sys::state& state, int32_t x, int32_t y, sys::key_modifiers mods) noexcept final;
-	void on_text(sys::state& state, char ch) noexcept final;
+	virtual void textbox_enter(sys::state& state, std::string_view s) noexcept { }
+	virtual void textbox_update(sys::state& state, std::string_view s) noexcept { }
+
+	message_result test_mouse(sys::state& state, int32_t x, int32_t y) noexcept override {
+		return message_result::consumed;
+	}
+	message_result on_lbutton_down(sys::state& state, int32_t x, int32_t y, sys::key_modifiers mods) noexcept override;
+	message_result on_key_down(sys::state& state, sys::virtual_key key, sys::key_modifiers mods) noexcept override;
+	void on_text(sys::state& state, char ch) noexcept override;
 	void render(sys::state& state, int32_t x, int32_t y) noexcept override;
 };
 
