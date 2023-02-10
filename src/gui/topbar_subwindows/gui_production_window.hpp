ee63d92b670a250fa2b60c5dd112a72628ed31b1
Masterchef118 <thelvadumee7162@gmail.com>
2023-02-10 18:23:10 -0500

    Added some windows to Production window and hid unused text elements.

--- src/gui/topbar_subwindows/gui_production_window.hpp
+++ src/gui/topbar_subwindows/gui_production_window.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "gui_element_types.hpp"
-#include "gui_investment_window.hpp"
+#include "gui_factory_buttons_window.hpp"
+#include "gui_invest_brow_window.hpp"
+#include "gui_invest_buttons_window.hpp"
 #include <vector>
 
 namespace ui {
@@ -19,7 +21,9 @@ public:
 		generic_tabbed_window::on_create(state);
 		set_visible(state, false);
 	}
-	investment_window* investwin = nullptr;
+	factory_buttons_window* factbutwin = nullptr;
+	invest_brow_window* investbrowwin = nullptr;
+	invest_buttons_window* investbutwin = nullptr;
 	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
 		if(name == "close_button") {
 			return make_element_by_type<generic_close_button>(state, id);
@@ -39,9 +43,36 @@ public:
 			auto ptr = make_element_by_type<generic_tab_button<production_window_tab>>(state, id);
 			ptr->target = production_window_tab::goods;
 			return ptr;
+		} else if(name == "tab_factories_text") {
+			auto ptr = make_element_by_type<simple_text_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "tab_invest_text") {
+			auto ptr = make_element_by_type<simple_text_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "tab_goodsproduction_text") {
+			auto ptr = make_element_by_type<simple_text_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "tab_popprojects_text") {
+			auto ptr = make_element_by_type<simple_text_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "factory_buttons") {
+			auto ptr = make_element_by_type<factory_buttons_window>(state, id);
+			factbutwin = ptr.get();
+			ptr->set_visible(state, true);
+			this->add_child_to_front(std::move(ptr));
+			return ptr;
 		} else if(name == "investment_browser") {
-			auto ptr = make_element_by_type<investment_window>(state, id);
-			investwin = ptr.get();
+			auto ptr = make_element_by_type<invest_brow_window>(state, id);
+			investbrowwin = ptr.get();
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "invest_buttons") {
+			auto ptr = make_element_by_type<invest_buttons_window>(state, id);
+			investbutwin = ptr.get();
 			ptr->set_visible(state, false);
 			return ptr;
 		} else {
@@ -63,8 +94,10 @@ public:
 
 	void hide_sub_windows(sys::state& state) {
 		hide_vector_elements(state, factory_elements);
+		factbutwin->set_visible(state, false);
 		hide_vector_elements(state, investment_elements);
-		investwin->set_visible(state, false);
+		investbrowwin->set_visible(state, false);
+		investbutwin->set_visible(state, false);
 		hide_vector_elements(state, project_elements);
 		hide_vector_elements(state, good_elements);
 	}
@@ -76,11 +109,15 @@ public:
 			switch(enum_val) {
 				case production_window_tab::factories:
 					show_vector_elements(state, factory_elements);
+					factbutwin->set_visible(state, true);
+					this->move_child_to_front(factbutwin);
 					break;
 				case production_window_tab::investments:
 					show_vector_elements(state, investment_elements);
-					investwin->set_visible(state, true);
-					this->move_child_to_front(investwin);
+					investbrowwin->set_visible(state, true);
+					this->move_child_to_front(investbrowwin);
+					investbutwin->set_visible(state, true);
+					this->move_child_to_front(investbutwin);
 					break;
 				case production_window_tab::projects:
 					show_vector_elements(state, project_elements);
