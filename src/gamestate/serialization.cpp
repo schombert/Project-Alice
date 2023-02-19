0de770a18d555d0c13f3df882914a0025fa3dec1
Schombert <pschombe+b@gmail.com>
2023-02-19 04:12:32 -0800

    Just build zstd along with everything else, I guess

--- src/gamestate/serialization.cpp
+++ src/gamestate/serialization.cpp
@@ -2,6 +2,11 @@
 #include "system_state.hpp"
 #include "serialization.hpp"
 
+#define ZSTD_STATIC_LINKING_ONLY
+#define XXH_NAMESPACE ZSTD_
+
+#include "zstd.h"
+
 namespace sys {
 
 uint8_t const* read_scenario_header(uint8_t const* ptr_in, scenario_header& header_out) {
@@ -39,20 +44,37 @@ size_t sizeof_save_header(save_header const& header_in) {
 	return sizeof(uint32_t) + sizeof(save_header);
 }
 
-
 uint8_t* write_compressed_section(uint8_t* ptr_out, uint8_t const* ptr_in, uint32_t uncompressed_size) {
-	// TODO: compress and serialize
-	uint32_t section_length = uncompressed_size; // <-- should be compressed size
 	uint32_t decompressed_length = uncompressed_size;
+
+	uint32_t section_length = uint32_t(
+		ZSTD_compress(ptr_out + sizeof(uint32_t) * 2, ZSTD_compressBound(uncompressed_size),
+			ptr_in, uncompressed_size, 10)); // write compressed data
 
 	memcpy(ptr_out, &section_length, sizeof(uint32_t));
 	memcpy(ptr_out + sizeof(uint32_t), &decompressed_length, sizeof(uint32_t));
 
-	memcpy(ptr_out + sizeof(uint32_t) * 2, ptr_in, section_length); // <-- change ptr in to the compressed memory
-
 	return ptr_out + sizeof(uint32_t) * 2 + section_length;
 }
 
+template<typename T>
+uint8_t const* with_decompressed_section(uint8_t const* ptr_in, T const& function) {
+	uint32_t section_length = 0;
+	uint32_t decompressed_length = 0;
+	memcpy(&section_length, ptr_in, sizeof(uint32_t));
+	memcpy(&decompressed_length, ptr_in + sizeof(uint32_t), sizeof(uint32_t));
+
+	uint8_t* temp_buffer = new uint8_t[decompressed_length];
+	//TODO: allocate memory for decompression and decompress into it
+
+	ZSTD_decompress(temp_buffer, decompressed_length, ptr_in + sizeof(uint32_t) * 2, section_length);
+
+	//function(ptr_in + sizeof(uint32_t) * 2, decompressed_length);
+	function(temp_buffer, decompressed_length);
+
+	delete[] temp_buffer;
+	return ptr_in + sizeof(uint32_t) * 2 + section_length;
+}
 
 uint8_t const* read_scenario_section(uint8_t const* ptr_in, uint8_t const* section_end, sys::state& state) {
 	// hand-written contribution
@@ -365,290 +387,6 @@ uint8_t* write_scenario_section(uint8_t* ptr_in, sys::state& state) {
 	}
 
 	dcon::load_record result = state.world.make_serialize_record_store_scenario();
-	//dcon::load_record result;
-	/*
-	result.national_identity = true;
-	result.national_identity_color = true;
-	result.national_identity_name = true;
-	result.national_identity_adjective = true;
-	result.national_identity_unit_names_first = true;
-	result.national_identity_unit_names_count = true;
-	result.national_identity_political_party_first = true;
-	result.national_identity_political_party_count = true;
-	result.national_identity_government_flag_type = true;
-	result.national_identity_primary_culture = true;
-	result.national_identity_religion = true;
-	result.national_identity_capital = true;
-	result.national_identity_is_releasable = true;
-	result.political_party = true;
-	result.political_party_name = true;
-	result.political_party_start_date = true;
-	result.political_party_end_date = true;
-	result.political_party_ideology = true;
-	result.political_party_party_issues = true;
-	result.religion = true;
-	result.religion_name = true;
-	result.religion_color = true;
-	result.religion_icon = true;
-	result.religion_is_pagan = true;
-	result.culture_group = true;
-	result.culture_group_name = true;
-	result.culture_group_is_overseas = true;
-	result.culture_group_leader = true;
-	result.culture = true;
-	result.culture_name = true;
-	result.culture_color = true;
-	result.culture_radicalism = true;
-	result.culture_first_names = true;
-	result.culture_last_names = true;
-	result.culture_group_membership = true;
-	result.culture_group_membership_member = true;
-	result.culture_group_membership_group = true;
-	result.cultural_union_of = true;
-	result.cultural_union_of_identity = true;
-	result.cultural_union_of_culture_group = true;
-	result.commodity = true;
-	result.commodity_name = true;
-	result.commodity_color = true;
-	result.commodity_cost = true;
-	result.commodity_commodity_group = true;
-	result.commodity_is_available_from_start = true;
-	result.commodity_is_mine = true;
-	result.commodity_rgo_amount = true;
-	result.commodity_rgo_workforce = true;
-	result.commodity_artisan_inputs = true;
-	result.commodity_artisan_output_amount = true;
-	result.modifier = true;
-	result.modifier_name = true;
-	result.modifier_province_values = true;
-	result.modifier_national_values = true;
-	result.modifier_icon = true;
-	result.factory_type = true;
-	result.factory_type_name = true;
-	result.factory_type_construction_costs = true;
-	result.factory_type_construction_time = true;
-	result.factory_type_is_available_from_start = true;
-	result.factory_type_is_coastal = true;
-	result.factory_type_inputs = true;
-	result.factory_type_efficiency_inputs = true;
-	result.factory_type_base_workforce = true;
-	result.factory_type_output = true;
-	result.factory_type_output_amount = true;
-	result.factory_type_bonus_1_trigger = true;
-	result.factory_type_bonus_1_amount = true;
-	result.factory_type_bonus_2_trigger = true;
-	result.factory_type_bonus_2_amount = true;
-	result.factory_type_bonus_3_trigger = true;
-	result.factory_type_bonus_3_amount = true;
-	result.ideology_group = true;
-	result.ideology_group_name = true;
-	result.ideology = true;
-	result.ideology_name = true;
-	result.ideology_color = true;
-	result.ideology_is_civilized_only = true;
-	result.ideology_activation_date = true;
-	result.ideology_add_political_reform = true;
-	result.ideology_remove_political_reform = true;
-	result.ideology_add_social_reform = true;
-	result.ideology_remove_social_reform = true;
-	result.ideology_add_military_reform = true;
-	result.ideology_add_economic_reform = true;
-	result.ideology_group_membership = true;
-	result.ideology_group_membership_ideology = true;
-	result.ideology_group_membership_ideology_group = true;
-	result.issue = true;
-	result.issue_name = true;
-	result.issue_options = true;
-	result.issue_is_next_step_only = true;
-	result.issue_is_administrative = true;
-	result.issue_option = true;
-	result.issue_option_name = true;
-	result.issue_option_rules = true;
-	result.issue_option_modifier = true;
-	result.issue_option_technology_cost = true;
-	result.issue_option_war_exhaustion_effect = true;
-	result.issue_option_administrative_multiplier = true;
-	result.issue_option_allow = true;
-	result.issue_option_on_execute_trigger = true;
-	result.issue_option_on_execute_effect = true;
-	result.cb_type = true;
-	result.cb_type_name = true;
-	result.cb_type_short_desc = true;
-	result.cb_type_long_desc = true;
-	result.cb_type_war_name = true;
-	result.cb_type_type_bits = true;
-	result.cb_type_months = true;
-	result.cb_type_truce_months = true;
-	result.cb_type_sprite_index = true;
-	result.cb_type_allowed_states = true;
-	result.cb_type_allowed_states_in_crisis = true;
-	result.cb_type_allowed_substate_regions = true;
-	result.cb_type_allowed_countries = true;
-	result.cb_type_can_use = true;
-	result.cb_type_on_add = true;
-	result.cb_type_on_po_accepted = true;
-	result.cb_type_badboy_factor = true;
-	result.cb_type_prestige_factor = true;
-	result.cb_type_peace_cost_factor = true;
-	result.cb_type_penalty_factor = true;
-	result.cb_type_break_truce_prestige_factor = true;
-	result.cb_type_break_truce_infamy_factor = true;
-	result.cb_type_break_truce_militancy_factor = true;
-	result.cb_type_good_relation_prestige_factor = true;
-	result.cb_type_good_relation_infamy_factor = true;
-	result.cb_type_good_relation_militancy_factor = true;
-	result.cb_type_construction_speed = true;
-	result.cb_type_tws_battle_factor = true;
-	result.leader_trait = true;
-	result.leader_trait_name = true;
-	result.leader_trait_organisation = true;
-	result.leader_trait_morale = true;
-	result.leader_trait_attack = true;
-	result.leader_trait_defence = true;
-	result.leader_trait_reconnaissance = true;
-	result.leader_trait_speed = true;
-	result.leader_trait_experience = true;
-	result.leader_trait_reliability = true;
-	result.pop_type = true;
-	result.pop_type_name = true;
-	result.pop_type_sprite = true;
-	result.pop_type_color = true;
-	result.pop_type_strata = true;
-	result.pop_type_everyday_needs_income_type = true;
-	result.pop_type_luxury_needs_income_type = true;
-	result.pop_type_life_needs_income_type = true;
-	result.pop_type_everyday_needs_income_weight = true;
-	result.pop_type_luxury_needs_income_weight = true;
-	result.pop_type_life_needs_income_weight = true;
-	result.pop_type_research_optimum = true;
-	result.pop_type_research_points = true;
-	result.pop_type_workplace_input = true;
-	result.pop_type_workplace_output = true;
-	result.pop_type_has_unemployment = true;
-	result.pop_type_migration_target = true;
-	result.pop_type_country_migration_target = true;
-	result.pop_type_issues = true;
-	result.pop_type_ideology = true;
-	result.pop_type_promotion = true;
-	result.pop_type_life_needs = true;
-	result.pop_type_everyday_needs = true;
-	result.pop_type_luxury_needs = true;
-	result.rebel_type = true;
-	result.rebel_type_name = true;
-	result.rebel_type_description = true;
-	result.rebel_type_army_name = true;
-	result.rebel_type_icon = true;
-	result.rebel_type_break_alliance_on_win = true;
-	result.rebel_type_area = true;
-	result.rebel_type_defection = true;
-	result.rebel_type_independence = true;
-	result.rebel_type_ideology = true;
-	result.rebel_type_occupation_multiplier = true;
-	result.rebel_type_defect_delay = true;
-	result.rebel_type_culture_restriction = true;
-	result.rebel_type_culture_group_restriction = true;
-	result.rebel_type_will_rise = true;
-	result.rebel_type_spawn_chance = true;
-	result.rebel_type_movement_evaluation = true;
-	result.rebel_type_siege_won_trigger = true;
-	result.rebel_type_siege_won_effect = true;
-	result.rebel_type_demands_enforced_trigger = true;
-	result.rebel_type_demands_enforced_effect = true;
-	result.rebel_type_government_change = true;
-	result.province = true;
-	result.province_continent = true;
-	result.province_climate = true;
-	result.province_adjacency = true;
-	result.province_adjacency_connected_provinces = true;
-	result.province_adjacency_distance = true;
-	result.state_definition = true;
-	result.abstract_state_membership = true;
-	result.abstract_state_membership_province = true;
-	result.abstract_state_membership_state = true;
-	result.technology = true;
-	result.technology_name = true;
-	result.technology_modifier = true;
-	result.technology_folder_index = true;
-	result.technology_year = true;
-	result.technology_ai_chance = true;
-	result.technology_cost = true;
-	result.technology_colonial_points = true;
-	result.technology_increase_railroad = true;
-	result.technology_increase_fort = true;
-	result.technology_increase_naval_base = true;
-	result.technology_activate_unit = true;
-	result.technology_activate_building = true;
-	result.technology_rgo_goods_output = true;
-	result.technology_factory_goods_output = true;
-	result.technology_rgo_size = true;
-	result.technology_modified_units = true;
-	result.invention = true;
-	result.invention_name = true;
-	result.invention_technology_type = true;
-	result.invention_modifier = true;
-	result.invention_chance = true;
-	result.invention_limit = true;
-	result.invention_enable_gas_attack = true;
-	result.invention_enable_gas_defence = true;
-	result.invention_shared_prestige = true;
-	result.invention_activate_unit = true;
-	result.invention_activate_crime = true;
-	result.invention_activate_building = true;
-	result.invention_rgo_goods_output = true;
-	result.invention_factory_goods_output = true;
-	result.invention_factory_goods_throughput = true;
-	result.invention_modified_units = true;
-	result.invention_rebel_org = true;
-	result.national_event = true;
-	result.national_event_name = true;
-	result.national_event_description = true;
-	result.national_event_is_major = true;
-	result.national_event_image_name = true;
-	result.national_event_immediate_effect = true;
-	result.national_event_options = true;
-	result.provincial_event = true;
-	result.provincial_event_name = true;
-	result.provincial_event_description = true;
-	result.provincial_event_image_name = true;
-	result.provincial_event_options = true;
-	result.free_national_event = true;
-	result.free_national_event_name = true;
-	result.free_national_event_description = true;
-	result.free_national_event_only_once = true;
-	result.free_national_event_is_major = true;
-	result.free_national_event_image_name = true;
-	result.free_national_event_trigger = true;
-	result.free_national_event_mtth = true;
-	result.free_national_event_immediate_effect = true;
-	result.free_national_event_options = true;
-	result.free_provincial_event = true;
-	result.free_provincial_event_name = true;
-	result.free_provincial_event_description = true;
-	result.free_provincial_event_only_once = true;
-	result.free_provincial_event_image_name = true;
-	result.free_provincial_event_trigger = true;
-	result.free_provincial_event_mtth = true;
-	result.free_provincial_event_options = true;
-	result.national_focus = true;
-	result.national_focus_name = true;
-	result.national_focus_icon = true;
-	result.national_focus_loyalty_value = true;
-	result.national_focus_ideology = true;
-	result.national_focus_limit = true;
-	result.national_focus_promotion_type = true;
-	result.national_focus_promotion_amount = true;
-	result.national_focus_immigrant_attract = true;
-	result.national_focus_railroads = true;
-	result.national_focus_production_focus = true;
-	result.decision = true;
-	result.decision_name = true;
-	result.decision_description = true;
-	result.decision_image_name = true;
-	result.decision_potential = true;
-	result.decision_allow = true;
-	result.decision_effect = true;
-	result.decision_ai_will_do = true;
-	*/
 	std::byte* start = reinterpret_cast<std::byte*>(ptr_in);
 	state.world.serialize(start, result);
 
@@ -860,7 +598,7 @@ void write_scenario_file(sys::state& state, native_string_view name) {
 	size_t save_space = sizeof_save_section(state);
 
 	// this is an upper bound, since compacting the data may require less space
-	size_t total_size = sizeof_scenario_header(header) + scenario_space + save_space
+	size_t total_size = sizeof_scenario_header(header) + ZSTD_compressBound(scenario_space) + ZSTD_compressBound(save_space)
 		+ sizeof(uint32_t) * 4;
 
 	uint8_t* temp_buffer = new uint8_t[total_size];
@@ -956,7 +694,7 @@ void write_save_file(sys::state& state, native_string_view name) {
 	size_t save_space = sizeof_save_section(state);
 
 	// this is an upper bound, since compacting the data may require less space
-	size_t total_size = sizeof_save_header(header) + save_space
+	size_t total_size = sizeof_save_header(header) + ZSTD_compressBound(save_space)
 		+ sizeof(uint32_t) * 2;
 
 	uint8_t* temp_buffer = new uint8_t[total_size];
