5a1115124ca20143a01dced100020f3d56f5685d
Schombert <pschombe+b@gmail.com>
2023-02-19 04:12:32 -0800

    Just build zstd along with everything else, I guess

--- src/main.cpp
+++ src/main.cpp
@@ -59,3 +59,29 @@ namespace sys {
 		// why does this exist ? So that the destructor of the unique pointer doesn't have to be known before it is implemented
 	}
 }
+
+// zstd
+
+#define XXH_NAMESPACE ZSTD_
+
+#include "zstd/xxhash.c"
+#include "zstd/zstd_decompress_block.c"
+#include "zstd/zstd_ddict.c"
+#include "zstd/huf_compress.c"
+#include "zstd/fse_compress.c"
+#include "zstd/huf_decompress.c"
+#include "zstd/fse_decompress.c"
+#include "zstd/zstd_common.c"
+#include "zstd/entropy_common.c"
+#include "zstd/hist.c"
+#include "zstd/zstd_compress_superblock.c"
+#include "zstd/zstd_ldm.c"
+#include "zstd/zstd_opt.c"
+#include "zstd/zstd_lazy.c"
+#include "zstd/zstd_double_fast.c"
+#include "zstd/zstd_fast.c"
+#include "zstd/zstd_compress_literals.c"
+#include "zstd/zstd_compress_sequences.c"
+#include "zstd/error_private.c"
+#include "zstd/zstd_decompress.c"
+#include "zstd/zstd_compress.c"
