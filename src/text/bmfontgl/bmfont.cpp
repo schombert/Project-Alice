f52dfdbbf482021529aba9a2d01d644b330aefd5
Masterchef118 <thelvadumee7162@gmail.com>
2023-03-11 13:52:35 -0500

    Fixed some compatibility issues with bmfontgl. Should be fully functional now.

--- src/text/bmfontgl/bmfont.cpp
+++ src/text/bmfontgl/bmfont.cpp
@@ -60,23 +60,6 @@ aaedev@gmail.com 2012
 
 #define MAX_BUFFER 256
 
-char *replace_str(char *str, char *orig, char *rep)
-{
-  static char buffer[MAX_BUFFER];
-  char *p;
-
-  if(!(p = strstr(str, orig)))  // Is 'orig' even in 'str'?
-    return str;
-
-  strncpy(buffer, str, p-str); // Copy characters from 'str' start to 'orig' st$
-  buffer[p-str] = '\0';
-
-  sprintf(buffer+(p-str), "%s%s", rep, p+strlen(orig));
-
-  return buffer;
-}
-
-
 // This structure holds the vertices for rendering with glDrawArrays
 typedef struct 
 {
@@ -86,7 +69,6 @@ typedef struct
 } vlist;                      
 
 vlist texlst[2048*4];
-
 
 bool BMFont::ParseFont(char *fontfile )
 {
@@ -246,7 +228,7 @@ float BMFont::GetStringWidth(const char *string)
   float total=0;
   CharDescriptor  *f;
 
-  for (int i = 0; i != strlen(string); i++)
+  for (uint8_t i = 0; i != strlen(string); i++)
    { 
 	  f=&Chars[string[i]];
       total+=f->XAdvance;
@@ -255,7 +237,7 @@ float BMFont::GetStringWidth(const char *string)
   return total * fscale;
 }
 
-GLuint LoadPNG(char* filename)
+GLuint LoadPNG(std::string filename)
 {
 
 	GLuint temptex;
@@ -352,12 +334,12 @@ std::vector<uint8_t> BMFont::LoadFontImage(char *fontfile, char* olddir, char* n
 	return buffer;
 }
 
-bool BMFont::MakePNG(char* fontfile, char* tgafile, std::vector<uint8_t> buffer) {
+bool BMFont::MakePNG(std::string fontfile, std::string tgafile, std::vector<uint8_t> buffer) {
 
-	LodePNG::encode(replace_str(tgafile, ".tga", ".png"), buffer, 256, 256);
+	LodePNG::encode(tgafile.replace(tgafile.size() - 4, tgafile.size(), ".png"), buffer, 256, 256);
 
 	//Ok, we have a file. Can we get the Texture as well?
-	char* buf = replace_str(fontfile, ".fnt", ".png");
+	std::string buf = tgafile.replace(fontfile.size() - 4, fontfile.size(), ".png");
 
 	ftexid = LoadPNG(buf);
 
@@ -366,7 +348,7 @@ bool BMFont::MakePNG(char* fontfile, char* tgafile, std::vector<uint8_t> buffer)
 
 bool BMFont::LoadFontfile(char* fontfile) {
 	ParseFont(fontfile);
-	KernCount = (int)Kearn.size();
+	KernCount = (short)Kearn.size();
 
 	return true;
 }
@@ -449,7 +431,7 @@ void BMFont::Print(float x, float y, const char *fmt, ...)
 	unsigned char *color = (unsigned char*)&fcolor;
 	
 	y= y + LineHeight;
-    Flen = strlen(text);
+    Flen = (int)strlen(text);
 
 	for (int i = 0; i != Flen; ++i)
 	{
@@ -510,7 +492,7 @@ void BMFont::Print(float x, float y, const char *fmt, ...)
 		  
 		 x +=  f->XAdvance;
     }
-   Render_String(strlen(text));
+   Render_String((int)strlen(text));
 }
 
 
@@ -521,7 +503,7 @@ void BMFont::PrintCenter( float y, const char *string)
 	
 	int window_width = 500;
 
-		int len = strlen(string);
+		int len = (int)strlen(string);
 
 		for (int i = 0; i != len; ++i)
 		{
