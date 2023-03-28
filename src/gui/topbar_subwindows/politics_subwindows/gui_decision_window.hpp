3c856d2cf190a62069c0f98e898e0ffb346f79c0
Jonathan Baker <jbak920@gmail.com>
2023-03-27 22:33:25 -0600

    Now there are always two blank decisions available

--- src/gui/topbar_subwindows/politics_subwindows/gui_decision_window.hpp
+++ src/gui/topbar_subwindows/politics_subwindows/gui_decision_window.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 #include "gui_element_types.hpp"
 
 namespace ui {
@@ -40,23 +42,38 @@ public:
 };
 
 class decision_listbox : public listbox_element_base<decision_item, dcon::decision_id> {
-public:
-	void on_create(sys::state& state) noexcept override {
-		// grab the list of decisions
-		// create & store a bunch of decision_item elements using those
-		// TODO: then also do this when things get updated
+protected:
+	dcon::nation_id current_nation{};
+  std::string_view get_row_element_name() override {
+    return "decision_entry";
 	}
+
+public:
+	void on_update(sys::state& state) noexcept override {}
 };
 
 class decision_window : public window_element_base {
 private:
   decision_listbox* decision_list{nullptr};
+	std::vector<dcon::decision_id> get_decisions() { 
+		//TODO: actually do this right
+		dcon::decision_id did1{ dcon::decision_id::value_base_t(10) }; // two random ids
+		dcon::decision_id did2{ dcon::decision_id::value_base_t(11) };
+		std::vector<dcon::decision_id> list; 
+		list.push_back(did1);
+		list.push_back(did2);
+		return list;
+	}
 
 public:
 	void on_create(sys::state& state) noexcept override {
 		window_element_base::on_create(state);
 		set_visible(state, false);
+		decision_list->row_contents = get_decisions();
+		decision_list->update(state);
 	}
+
+	//TODO: add when else to call get_decisions. on_update? set? get?
 
 	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
 		if(name == "decision_listbox") {
