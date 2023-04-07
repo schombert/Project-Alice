d03cf54fb3beacb6153375d8258f3c0f10a424cf
nuruvilu <nuruvilu@konpaku.tech>
2023-04-07 02:50:55 -0500

    Implement reforms windows
    Also added next election start date to government description text.

--- src/culture/politics.hpp
+++ src/culture/politics.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "date_interface.hpp"
 #include "dcon_generated.hpp"
 #include "system_state.hpp"
+#include <string_view>
 
 namespace politics {
 
@@ -13,5 +15,11 @@ float get_voter_support(sys::state& state, dcon::nation_id nation, dcon::issue_o
 bool can_appoint_ruling_party(sys::state& state, dcon::nation_id nation);
 bool is_election_ongoing(sys::state& state, dcon::nation_id nation);
 bool has_elections(sys::state& state, dcon::nation_id nation);
+sys::date next_election_date(sys::state& state, dcon::nation_id nation);
+
+dcon::reform_id get_reform_by_name(sys::state& state, std::string_view name);
+dcon::issue_id get_issue_by_name(sys::state& state, std::string_view name);
+bool reform_is_selected(sys::state& state, dcon::nation_id nation, dcon::reform_option_id reform_option);
+bool issue_is_selected(sys::state& state, dcon::nation_id nation, dcon::issue_option_id issue_option);
 
 }
