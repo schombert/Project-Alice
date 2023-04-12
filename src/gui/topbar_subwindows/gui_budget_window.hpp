65eceb58ce5eacc8711c809c66930bb70f54df57
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-04-12 10:10:27 +0800

    Add tarriffs % on budget window

--- src/gui/topbar_subwindows/gui_budget_window.hpp
+++ src/gui/topbar_subwindows/gui_budget_window.hpp
@@ -4,6 +4,14 @@
 
 namespace ui {
 
+class budget_tariffs_percent : public simple_text_element_base {
+public:
+	void on_update(sys::state& state) noexcept override {
+		auto tariffs = state.world.nation_get_tariffs(state.local_player_nation);
+		set_text(state, std::to_string(tariffs) + "%");
+	}
+};
+
 class budget_window : public window_element_base {
 public:
 	void on_create(sys::state& state) noexcept override {
@@ -14,6 +22,8 @@ public:
 	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
 		if(name == "close_button") {
 			return make_element_by_type<generic_close_button>(state, id);
+		} else if(name == "tariffs_percent") {
+			return make_element_by_type<budget_tariffs_percent>(state, id);
 		} else {
 			return nullptr;
 		}
