c4e1b045937d62b6a89e54b1969d0296f250583e
Masterchef118 <thelvadumee7162@gmail.com>
2023-04-14 17:55:03 -0400

    Added names for list of generals, though they are partially broken atm.

--- src/gui/topbar_subwindows/military_subwindows/gui_leaders_window.hpp
+++ src/gui/topbar_subwindows/military_subwindows/gui_leaders_window.hpp
@@ -6,6 +6,8 @@ namespace ui {
 
 class military_leaders : public listbox_row_element_base<dcon::general_id> {
 public:
+	dcon::general_id leader_id;
+	ui::simple_text_element_base* leader_name;
 	void on_create(sys::state& state) noexcept override {
 		listbox_row_element_base::on_create(state);
 		this->base_data.position.y = 0;
@@ -14,7 +16,10 @@ public:
 
 	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
 		if(name == "name") {
-			return make_element_by_type<simple_text_element_base>(state, id);
+			auto ptr = make_element_by_type<simple_text_element_base>(state, id);
+			leader_name = ptr.get();
+			ptr->set_visible(state, true);
+			return ptr;
 		} else if(name == "background") {
 			return make_element_by_type<simple_text_element_base>(state, id);
 		} else {
@@ -22,13 +27,17 @@ public:
 		}
 	}
 
-	//message_result get(sys::state& state, Cyto::Any& payload) noexcept override {
-		//if(payload.holds_type<dcon::state_instance_id>()) {
-			//impl_set(state, payload);
-			//return message_result::consumed;
-		//}
-		//return message_result::unseen;
-	//}
+	void update_name(sys::state& state) {
+		leader_name->set_text(state, std::string(state.to_string_view(state.world.general_get_name(this->leader_id))));
+	}
+
+	message_result get(sys::state& state, Cyto::Any& payload) noexcept override {
+		if(payload.holds_type<dcon::general_id>()) {
+			impl_set(state, payload);
+			return message_result::consumed;
+		}
+		return message_result::unseen;
+	}
 };
 
 class military_leaders_listbox : public listbox_element_base<military_leaders, dcon::general_id> {
@@ -39,8 +48,15 @@ protected:
 public:
 	void on_update(sys::state& state) noexcept override {
 		row_contents.clear();
-		for(const auto fat_id : state.world.nation_get_general_loyalty(state.local_player_nation))
+		int i = 0;
+		for(const auto fat_id : state.world.nation_get_general_loyalty(state.local_player_nation)) {
 			row_contents.push_back(fat_id.get_leader());
+			if(size_t(i) < this->row_windows.size()) {
+				this->row_windows[i]->leader_id = fat_id.get_leader();
+				this->row_windows[i]->update_name(state);
+				i++;
+			}
+		}
 		update(state);
 	}
 };
