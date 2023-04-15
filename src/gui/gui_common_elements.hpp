fc9721d1bbe3a363c2820754f35a7f020fa691eb
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-04-15 12:23:36 +0800

    units listbox in military window

--- src/gui/gui_common_elements.hpp
+++ src/gui/gui_common_elements.hpp
@@ -144,6 +144,16 @@ public:
 	}
 };
 
+class standard_unit_progress_bar : public progress_bar {
+public:
+    void on_create(sys::state& state) noexcept override {
+        std::swap(base_data.size.x, base_data.size.y);
+        base_data.position.x -= base_data.size.x;
+		base_data.position.y -= base_data.size.y;
+		base_data.position.y += 4; // Nudge-inplace
+    }
+};
+
 class standard_province_progress_bar : public progress_bar {
 protected:
 	dcon::province_id prov_id{};
