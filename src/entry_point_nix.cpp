698028ea6695016aa3726a9758d6d449d3ffe4c9
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-04-18 01:09:38 +0800

    be able to pass mod files thru command line (linux only)

--- src/entry_point_nix.cpp
+++ src/entry_point_nix.cpp
@@ -1,13 +1,29 @@
 #include "system_state.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
 	std::unique_ptr<sys::state> game_state = std::make_unique<sys::state>(); // too big for the stack
 
 	assert(std::string("NONE") != GAME_DIR); // If this fails, then you have not created a local_user_settings.hpp (read the documentation for contributors)
 	add_root(game_state->common_fs, NATIVE_M(GAME_DIR)); // game files directory is overlaid on top of that
 	add_root(game_state->common_fs, NATIVE(".")); // will add the working directory as first root -- for the moment this lets us find the shader files
-	
 
+	{
+		std::vector<std::string> cmd_args;
+		for(int i = 1; i < argc; ++i)
+			cmd_args.push_back(std::string{ argv[i] });
+
+		parsers::error_handler err("");
+		parsers::scenario_building_context context(*game_state);
+		auto root = get_root(game_state->common_fs);
+		for(const auto& mod_filename : cmd_args) {
+			auto mod_file = open_file(root, mod_filename);
+			auto content = view_contents(*mod_file);
+			err.file_name = mod_filename;
+			parsers::token_generator gen(content.data, content.data + content.file_size);
+			parsers::mod_file_context mod_file_context(context);
+			parsers::parse_mod_file(gen, err, mod_file_context);
+		}
+	}
 
 	if(!sys::try_read_scenario_and_save_file(*game_state, NATIVE("development_test_file.bin"))) {
 		// scenario making functions
