18f972bb29ec3a7123011446f7505b63f5704edd
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-04-19 09:37:19 +0800

    use_reusable_event_window

--- src/gui/gui_event.cpp
+++ src/gui/gui_event.cpp
@@ -2,28 +2,27 @@
 
 namespace ui {
 template<typename V>
-static bool find_usable_window(sys::state& state, V& v, Cyto::Any& payload) {
-	// Try to find an usable window first...
-	for(auto it = v.begin(); it != v.end(); it++) {
-		(*it)->impl_set(state, payload);
-		state.ui_state.root->add_child_to_front(std::move(*it));
-		v.erase(it);
-		return true;
-	}
-	return false;
+static bool use_reusable_event_window(sys::state& state, V& v, Cyto::Any& payload) {
+	if(v.empty())
+		return false;
+	auto it = v.begin();
+	(*it)->impl_set(state, payload);
+	state.ui_state.root->add_child_to_front(std::move(*it));
+	v.erase(it);
+	return true;
 }
 template<typename T>
 static void fire_event_national_internal(sys::state& state, T fat_id) {
 	Cyto::Any payload = ui::national_event_data_wrapper(fat_id.id);
 	if(fat_id.get_is_major()) {
-		if(find_usable_window(state, state.ui_state.spare_major_event_subwindows, payload))
+		if(use_reusable_event_window(state, state.ui_state.spare_major_event_subwindows, payload))
 			return;
 		// Otherwise add it to the window list
 		auto new_elm = ui::make_element_by_type<ui::national_event_window<true>>(state, "event_major_window");
 		new_elm->impl_set(state, payload);
 		state.ui_state.root->add_child_to_front(std::move(new_elm));
 	} else {
-		if(find_usable_window(state, state.ui_state.spare_national_event_subwindows, payload))
+		if(use_reusable_event_window(state, state.ui_state.spare_national_event_subwindows, payload))
 			return;
 		// Otherwise add it to the window list
 		auto new_elm = ui::make_element_by_type<ui::national_event_window<false>>(state, "event_country_window");
@@ -34,7 +33,7 @@ static void fire_event_national_internal(sys::state& state, T fat_id) {
 template<typename T>
 static void fire_event_provincial_internal(sys::state& state, T fat_id) {
 	Cyto::Any payload = ui::provincial_event_data_wrapper(fat_id.id);
-	if(find_usable_window(state, state.ui_state.spare_provincial_event_subwindows, payload))
+	if(use_reusable_event_window(state, state.ui_state.spare_provincial_event_subwindows, payload))
 		return;
 	// Otherwise add it to the window list
 	auto new_elm = ui::make_element_by_type<ui::provincial_event_window>(state, "event_province_window");
