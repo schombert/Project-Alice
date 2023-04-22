7760ebc8bbcf4be5b455657e7b32880a0f75727d
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-04-23 02:06:07 +0800

    fix colours

--- src/gui/gui_map_icons.hpp
+++ src/gui/gui_map_icons.hpp
@@ -26,9 +26,9 @@ class unit_panel_color : public generic_settable_element<image_element_base, dco
         return 2; // gray, i.e not involved or irrelevant for us
     }
 public:
-	void on_update(sys::state& state) noexcept override {
+    void on_update(sys::state& state) noexcept override {
         frame = get_frame(state);
-	}
+    }
 };
 
 typedef std::variant<
@@ -37,19 +37,19 @@ typedef std::variant<
 > unit_strength_wrapper;
 class unit_strength_text : public generic_settable_element<simple_text_element_base, unit_strength_wrapper> {
 public:
-	void on_update(sys::state& state) noexcept override {
+    void on_update(sys::state& state) noexcept override {
         if(std::holds_alternative<int32_t>(content))
             set_text(state, std::to_string(std::get<int32_t>(content)));
         else
             set_text(state, text::prettify(int32_t(std::get<float>(content))));
-	}
+    }
 };
 
 class unit_country_flag : public generic_settable_element<flag_button, dcon::nation_id> {
 public:
-	dcon::national_identity_id get_current_nation(sys::state& state) noexcept override {
-		return state.world.nation_get_identity_from_identity_holder(content);
-	}
+    dcon::national_identity_id get_current_nation(sys::state& state) noexcept override {
+        return state.world.nation_get_identity_from_identity_holder(content);
+    }
 
     void on_update(sys::state& state) noexcept override {
         set_current_nation(state, get_current_nation(state));
@@ -60,24 +60,24 @@ class unit_icon_window : public generic_settable_element<window_element_base, dc
     unit_strength_text* strength_text = nullptr;
     unit_country_flag* country_flag = nullptr;
 public:
-	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
-		if(name == "unit_panel_country_flag") {
-			auto ptr = make_element_by_type<unit_country_flag>(state, id);
+    std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
+        if(name == "unit_panel_country_flag") {
+            auto ptr = make_element_by_type<unit_country_flag>(state, id);
             ptr->base_data.position.y -= 1; // Nudge
             country_flag = ptr.get();
             return ptr;
         } else if(name == "unit_panel_color")
-			return make_element_by_type<unit_panel_color>(state, id);
+            return make_element_by_type<unit_panel_color>(state, id);
         else if(name == "unit_strength") {
-			auto ptr = make_element_by_type<unit_strength_text>(state, id);
+            auto ptr = make_element_by_type<unit_strength_text>(state, id);
             ptr->base_data.position.y -= 1; // Nudge
             strength_text = ptr.get();
             return ptr;
         } else
-			return nullptr;
-	}
+            return nullptr;
+    }
 
-	void on_update(sys::state& state) noexcept override {
+    void on_update(sys::state& state) noexcept override {
         // Idempotency prohibits the widget from setting too much the elements into
         // otherwise redundant data, so only the first unit on a province will be shown
         // and only the first ship will be shown. Ships are given priority over armies.
@@ -90,7 +90,7 @@ public:
                 auto nid = state.world.navy_location_get_navy(id);
                 if(!idempotency) {
                     Cyto::Any payload = state.world.navy_control_get_controller(state.world.navy_get_navy_control_as_navy(nid));
-                    country_flag->impl_set(state, payload);
+                    impl_set(state, payload);
                 }
                 idempotency = has_navy = true;
                 // Calculate strength
@@ -108,7 +108,7 @@ public:
                 auto aid = state.world.army_location_get_army(id);
                 if(!idempotency) {
                     Cyto::Any payload = state.world.army_control_get_controller(state.world.army_get_army_control_as_army(aid));
-                    country_flag->impl_set(state, payload);
+                    impl_set(state, payload);
                 }
                 idempotency = has_army = true;
                 // Calculate strength
@@ -121,15 +121,15 @@ public:
             strength_text->impl_set(state, payload);
         }
         set_visible(state, has_navy || has_army);
-	}
+    }
 
-	void render(sys::state& state, int32_t x, int32_t y) noexcept override {
+    void render(sys::state& state, int32_t x, int32_t y) noexcept override {
         const auto mid_point = state.world.province_get_mid_point(content);
         const glm::vec2 screen_size{ float(state.x_size), float(state.y_size) };
         const auto screen_pos = state.map_display.map_to_screen(state.map_display.normalize_map_coord(mid_point), screen_size);
-		base_data.position.x = screen_pos.x;
+        base_data.position.x = screen_pos.x;
         base_data.position.y = screen_pos.y;
         window_element_base::render(state, 0, 0);
-	}
+    }
 };
 }
