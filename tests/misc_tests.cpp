e50101c3ba68210b92fec322faab56c3c9f50977
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-05-22 09:21:15 +0800

    more testcases for cyto::any

--- tests/misc_tests.cpp
+++ tests/misc_tests.cpp
@@ -63,7 +63,7 @@ TEST_CASE("date tests", "[misc_tests]") {
 TEST_CASE("cyto payload tests", "[misc_tests]") {
 	SECTION("int_emplace") {
 		Cyto::Any payload = int(64);
-		REQUIRE(payload.holds_type<int>() == true);
+		REQUIRE(payload.holds_type<int>());
 		REQUIRE(any_cast<int>(payload) == 64);
 	}
     SECTION("same_struct_type_diff") {
@@ -92,5 +92,136 @@ TEST_CASE("cyto payload tests", "[misc_tests]") {
 		REQUIRE(!payload.holds_type<ctest_c2>());
 		REQUIRE(payload.holds_type<int>());
 		REQUIRE(any_cast<int>(payload) == 12);
+	}
+	SECTION("primitives_test") {
+		Cyto::Any c_payload = char(124);
+		REQUIRE(c_payload.holds_type<char>());
+		REQUIRE(!c_payload.holds_type<short>());
+		REQUIRE(!c_payload.holds_type<int>());
+		REQUIRE(!c_payload.holds_type<long>());
+		REQUIRE(!c_payload.holds_type<long long>());
+		REQUIRE(!c_payload.holds_type<float>());
+		REQUIRE(!c_payload.holds_type<double>());
+		REQUIRE(!c_payload.holds_type<void*>());
+		REQUIRE(!c_payload.holds_type<char*>());
+		REQUIRE(!c_payload.holds_type<short*>());
+		REQUIRE(!c_payload.holds_type<int*>());
+		REQUIRE(!c_payload.holds_type<long*>());
+		REQUIRE(!c_payload.holds_type<long long*>());
+		REQUIRE(!c_payload.holds_type<float*>());
+		REQUIRE(!c_payload.holds_type<double*>());
+		REQUIRE(any_cast<char>(c_payload) == 124);
+
+		Cyto::Any s_payload = short(0xff);
+		REQUIRE(!s_payload.holds_type<char>());
+		REQUIRE(s_payload.holds_type<short>());
+		REQUIRE(!s_payload.holds_type<int>());
+		REQUIRE(!s_payload.holds_type<long>());
+		REQUIRE(!s_payload.holds_type<long long>());
+		REQUIRE(!s_payload.holds_type<float>());
+		REQUIRE(!s_payload.holds_type<double>());
+		REQUIRE(!s_payload.holds_type<void*>());
+		REQUIRE(!s_payload.holds_type<char*>());
+		REQUIRE(!s_payload.holds_type<short*>());
+		REQUIRE(!s_payload.holds_type<int*>());
+		REQUIRE(!s_payload.holds_type<long*>());
+		REQUIRE(!s_payload.holds_type<long long*>());
+		REQUIRE(!s_payload.holds_type<float*>());
+		REQUIRE(!s_payload.holds_type<double*>());
+		REQUIRE(any_cast<short>(s_payload) == 0xff);
+		
+		Cyto::Any i_payload = int(1);
+		REQUIRE(!i_payload.holds_type<char>());
+		REQUIRE(!i_payload.holds_type<short>());
+		REQUIRE(i_payload.holds_type<int>());
+		REQUIRE(!i_payload.holds_type<long>());
+		REQUIRE(!i_payload.holds_type<long long>());
+		REQUIRE(!i_payload.holds_type<float>());
+		REQUIRE(!i_payload.holds_type<double>());
+		REQUIRE(!i_payload.holds_type<void*>());
+		REQUIRE(!i_payload.holds_type<char*>());
+		REQUIRE(!i_payload.holds_type<short*>());
+		REQUIRE(!i_payload.holds_type<int*>());
+		REQUIRE(!i_payload.holds_type<long*>());
+		REQUIRE(!i_payload.holds_type<long long*>());
+		REQUIRE(!i_payload.holds_type<float*>());
+		REQUIRE(!i_payload.holds_type<double*>());
+		REQUIRE(any_cast<int>(i_payload) == 1);
+
+		Cyto::Any l_payload = long(0xff);
+		REQUIRE(!l_payload.holds_type<char>());
+		REQUIRE(!l_payload.holds_type<short>());
+		REQUIRE(!l_payload.holds_type<int>());
+		REQUIRE(l_payload.holds_type<long>());
+		REQUIRE(!l_payload.holds_type<long long>());
+		REQUIRE(!l_payload.holds_type<float>());
+		REQUIRE(!l_payload.holds_type<double>());
+		REQUIRE(!l_payload.holds_type<void*>());
+		REQUIRE(!l_payload.holds_type<char*>());
+		REQUIRE(!l_payload.holds_type<short*>());
+		REQUIRE(!l_payload.holds_type<int*>());
+		REQUIRE(!l_payload.holds_type<long*>());
+		REQUIRE(!l_payload.holds_type<long long*>());
+		REQUIRE(!l_payload.holds_type<float*>());
+		REQUIRE(!l_payload.holds_type<double*>());
+		REQUIRE(any_cast<long>(l_payload) == 0xff);
+	}
+
+	SECTION("floating_primitives_test") {
+		Cyto::Any f_payload = float(421.421f);
+		REQUIRE(!f_payload.holds_type<char>());
+		REQUIRE(!f_payload.holds_type<short>());
+		REQUIRE(!f_payload.holds_type<int>());
+		REQUIRE(!f_payload.holds_type<long>());
+		REQUIRE(!f_payload.holds_type<long long>());
+		REQUIRE(f_payload.holds_type<float>());
+		REQUIRE(!f_payload.holds_type<double>());
+		REQUIRE(!f_payload.holds_type<void*>());
+		REQUIRE(!f_payload.holds_type<char*>());
+		REQUIRE(!f_payload.holds_type<short*>());
+		REQUIRE(!f_payload.holds_type<int*>());
+		REQUIRE(!f_payload.holds_type<long*>());
+		REQUIRE(!f_payload.holds_type<long long*>());
+		REQUIRE(!f_payload.holds_type<float*>());
+		REQUIRE(!f_payload.holds_type<double*>());
+		REQUIRE(any_cast<float>(f_payload) == 421.421f);
+
+		Cyto::Any d_payload = double(69.45f);
+		REQUIRE(!d_payload.holds_type<char>());
+		REQUIRE(!d_payload.holds_type<short>());
+		REQUIRE(!d_payload.holds_type<int>());
+		REQUIRE(!d_payload.holds_type<long>());
+		REQUIRE(!d_payload.holds_type<long long>());
+		REQUIRE(!d_payload.holds_type<float>());
+		REQUIRE(d_payload.holds_type<double>());
+		REQUIRE(!d_payload.holds_type<void*>());
+		REQUIRE(!d_payload.holds_type<char*>());
+		REQUIRE(!d_payload.holds_type<short*>());
+		REQUIRE(!d_payload.holds_type<int*>());
+		REQUIRE(!d_payload.holds_type<long*>());
+		REQUIRE(!d_payload.holds_type<long long*>());
+		REQUIRE(!d_payload.holds_type<float*>());
+		REQUIRE(!d_payload.holds_type<double*>());
+		REQUIRE(any_cast<double>(d_payload) == 69.45f);
+	}
+
+	SECTION("pointers_test") {
+		Cyto::Any vp_payload = (void*)nullptr;
+		REQUIRE(!vp_payload.holds_type<char>());
+		REQUIRE(!vp_payload.holds_type<short>());
+		REQUIRE(!vp_payload.holds_type<int>());
+		REQUIRE(!vp_payload.holds_type<long>());
+		REQUIRE(!vp_payload.holds_type<long long>());
+		REQUIRE(!vp_payload.holds_type<float>());
+		REQUIRE(!vp_payload.holds_type<double>());
+		REQUIRE(vp_payload.holds_type<void*>());
+		REQUIRE(!vp_payload.holds_type<char*>());
+		REQUIRE(!vp_payload.holds_type<short*>());
+		REQUIRE(!vp_payload.holds_type<int*>());
+		REQUIRE(!vp_payload.holds_type<long*>());
+		REQUIRE(!vp_payload.holds_type<long long*>());
+		REQUIRE(!vp_payload.holds_type<float*>());
+		REQUIRE(!vp_payload.holds_type<double*>());
+		REQUIRE(any_cast<void*>(vp_payload) == (void*)nullptr);
 	}
 }
