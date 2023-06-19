fc8b89d13b73c8421dc756c9b217805aa65de22d
Schombert <pschombe+b@gmail.com>
2023-06-19 16:59:13 -0700

    politics window

--- src/text/text.hpp
+++ src/text/text.hpp
@@ -684,6 +684,8 @@ void add_line(sys::state& state, layout_base& dest, std::string_view key, variab
 		int32_t indent = 0);
 void add_line(sys::state& state, layout_base& dest, std::string_view key, variable_type subkey, substitution value,
 		variable_type subkey_b, substitution value_b, int32_t indent = 0);
+void add_line(sys::state& state, layout_base& dest, std::string_view key, variable_type subkey, substitution value,
+		variable_type subkey_b, substitution value_b, variable_type subkey_c, substitution value_c, int32_t indent = 0);
 void add_line_with_condition(sys::state& state, layout_base& dest, std::string_view key, bool condition_met, int32_t indent = 0);
 
 void add_divider_to_layout_box(sys::state& state, layout_base& dest, layout_box& box);
