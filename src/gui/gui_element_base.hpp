98fc484848f4f64a25de27f94f932e1fa4a2eeb9
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-08-01 00:24:47 +0800

    fix uwu

--- src/gui/gui_element_base.hpp
+++ src/gui/gui_element_base.hpp
@@ -118,11 +118,13 @@ public:
 
 template<typename T>
 inline T retrieve(sys::state& state, element_base* parent) {
-	Cyto::Any payload = T{};
 	if(parent) {
+		Cyto::Any payload = T{};
 		parent->impl_get(state, payload);
+		return any_cast<T>(payload);
+	} else {
+		return T{};
 	}
-	return any_cast<T>(payload);
 }
 
 template<typename T>
