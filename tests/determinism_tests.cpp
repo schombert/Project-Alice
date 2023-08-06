941b522af5e0b47f3664eee156701cde0eed3491
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-08-07 05:27:42 +0800

    float fmadd and stuff determinism tests

---
+++ tests/determinism_tests.cpp
@@ -0,0 +1,74 @@
+#include "catch.hpp"
+#include "parsers_declarations.hpp"
+#include "dcon_generated.hpp"
+#include "nations.hpp"
+#include "container_types.hpp"
+#include "system_state.hpp"
+#include "serialization.hpp"
+#include "prng.hpp"
+
+TEST_CASE("prng_simple", "[determinism]") {
+	std::unique_ptr<sys::state> game_state = std::make_unique<sys::state>(); // too big for the stack
+	game_state->game_seed = 64273;
+	game_state->current_date.value = 49963;
+	auto r1 = rng::get_random(*game_state, ~0x6a3f);
+	auto r2 = rng::get_random(*game_state, ~0x6a3f);
+	REQUIRE(r1 == r2);
+}
+
+TEST_CASE("prng_low_entropy", "[determinism]") {
+	std::unique_ptr<sys::state> game_state = std::make_unique<sys::state>(); // too big for the stack
+	game_state->game_seed = 1;
+	game_state->current_date.value = 1;
+	auto r1 = rng::get_random(*game_state, 1);
+	auto r2 = rng::get_random(*game_state, 1);
+	REQUIRE(r1 == r2);
+}
+
+#define UNOPTIMIZABLE_FLOAT(name, value) \
+	char name##_storage[sizeof(float)]; \
+	new (&name##_storage) float(value); \
+	const float *name##_p = std::launder(reinterpret_cast<const float*>(&name##_storage)); \
+	const float name = *name##_p;
+
+TEST_CASE("fp_mul", "[determinism]") {
+	UNOPTIMIZABLE_FLOAT(f1, 2.f);
+	UNOPTIMIZABLE_FLOAT(f2, 4.f);
+	REQUIRE(f1 * f2 == 8.f);
+}
+TEST_CASE("fp_fmadd", "[determinism]") {
+	UNOPTIMIZABLE_FLOAT(f1, 2.f);
+	UNOPTIMIZABLE_FLOAT(f2, 4.f);
+	UNOPTIMIZABLE_FLOAT(f3, 3.f);
+	UNOPTIMIZABLE_FLOAT(f4, 2.f);
+	
+	float fmadd_1 = f1 * f2 + f3 * f4; //equation
+	float fmadd_2 = f3 * f4 + f1 * f2; //reverse products
+	float fmadd_3 = f1 * (f2 + (f3 * f4) / f1); //factorization by f1
+	float fmadd_4 = f2 * (f1 + (f3 * f4) / f2); //factorization by f2
+	float fmadd_5 = f3 * ((f1 * f2) / f3 + f4); //factorization by f3
+	float fmadd_6 = f4 * ((f1 * f2) / f4 + f3); //factorization by f4
+
+	REQUIRE(fmadd_1 == fmadd_2);
+	REQUIRE(fmadd_1 == fmadd_3);
+	REQUIRE(fmadd_1 == fmadd_4);
+	REQUIRE(fmadd_1 == fmadd_5);
+	REQUIRE(fmadd_1 == fmadd_6);
+}
+
+TEST_CASE("fp_fprec", "[determinism]") {
+	UNOPTIMIZABLE_FLOAT(f1, 2.f);
+	UNOPTIMIZABLE_FLOAT(f2, 4.f);
+	UNOPTIMIZABLE_FLOAT(f3, 3.f);
+	
+	float fprec_1 = f1 * f2 / f3; //equation
+	float fprec_2 = (f1 * f2) / f3; //same
+	float fprec_3 = f1 * (f2 / f3); //same
+	float fprec_4 = (f1 / f3) * f2; //same
+
+	REQUIRE(fprec_1 == fprec_2);
+	REQUIRE(fprec_1 == fprec_3);
+	REQUIRE(fprec_1 == fprec_4);
+}
+
+#undef UNOPTIMIZABLE_FLOAT
