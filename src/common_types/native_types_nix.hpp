3008e40174ba8beeaa745812cb6ddf741522bda1
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-08-12 23:54:03 +0800

    implement to_native_string

--- src/common_types/native_types_nix.hpp
+++ src/common_types/native_types_nix.hpp
@@ -5,6 +5,10 @@
 using native_string = std::string;
 using native_string_view = std::string_view;
 using native_char = char;
+template<typename T>
+native_string to_native_string(T&& v) noexcept {
+    return std::to_string(v);
+}
 
 #define NATIVE(X) X
 #define NATIVE_M(X) NATIVE(X)
