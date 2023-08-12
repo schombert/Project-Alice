5cfe59273e61c1820facc6688a8518499065a12c
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-08-13 02:49:08 +0800

    chat window draft

--- src/gui/gui_minimap.hpp
+++ src/gui/gui_minimap.hpp
@@ -305,6 +305,35 @@ public:
 	}
 };
 
+class minimap_chat_window : public window_element_base {
+public:
+	std::unique_ptr<element_base> make_child(sys::state& state, std::string_view name, dcon::gui_def_id id) noexcept override {
+		if(name == "closebutton") {
+			return make_element_by_type<generic_close_button>(state, id);
+		} else if(name == "chat_bg") {
+			return make_element_by_type<image_element_base>(state, id);
+		} else if(name == "none") {
+			auto ptr = make_element_by_type<button_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "allies") {
+			auto ptr = make_element_by_type<button_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "enemies") {
+			auto ptr = make_element_by_type<button_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else if(name == "all") {
+			auto ptr = make_element_by_type<button_element_base>(state, id);
+			ptr->set_visible(state, false);
+			return ptr;
+		} else {
+			return nullptr;
+		}
+	}
+};
+
 class minimap_container_window : public window_element_base {
 	const std::string_view mapmode_btn_prefix{"mapmode_"};
 
@@ -341,6 +370,8 @@ public:
 			return make_element_by_type<minimap_msg_event_button>(state, id);
 		} else if(name == "menubar_msg_other") {
 			return make_element_by_type<minimap_msg_other_button>(state, id);
+		} else if(name == "chat_window") {
+			return make_element_by_type<minimap_chat_window>(state, id);
 		} else if(name.starts_with(mapmode_btn_prefix)) {
 			auto ptr = make_element_by_type<minimap_mapmode_button>(state, id);
 			size_t num_index = name.rfind("_") + 1;
