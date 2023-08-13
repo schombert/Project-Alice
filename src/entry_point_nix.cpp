bbe13f524a4b49f273c99abdf96e6e92a7b9da6d
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-08-13 11:21:19 +0800

    use static sys::state instead of it being allocated

--- src/entry_point_nix.cpp
+++ src/entry_point_nix.cpp
@@ -1,36 +1,36 @@
 #include "system_state.hpp"
 
-int main() {
-	std::unique_ptr<sys::state> game_state = std::make_unique<sys::state>(); // too big for the stack
+static sys::state game_state; // too big for the stack
 
+int main() {
 	assert(
 			std::string("NONE") !=
 			GAME_DIR); // If this fails, then you have not created a local_user_settings.hpp (read the documentation for contributors)
-	add_root(game_state->common_fs, NATIVE_M(GAME_DIR)); // game files directory is overlaid on top of that
-	add_root(game_state->common_fs,
+	add_root(game_state.common_fs, NATIVE_M(GAME_DIR)); // game files directory is overlaid on top of that
+	add_root(game_state.common_fs,
 			NATIVE(".")); // will add the working directory as first root -- for the moment this lets us find the shader files
 
-	if(!sys::try_read_scenario_and_save_file(*game_state, NATIVE("development_test_file.bin"))) {
+	if(!sys::try_read_scenario_and_save_file(game_state, NATIVE("development_test_file.bin"))) {
 		// scenario making functions
-		game_state->load_scenario_data();
-		game_state->local_player_nation = dcon::nation_id{8};
-		game_state->world.nation_set_is_player_controlled(game_state->local_player_nation, true);
-		sys::write_scenario_file(*game_state, NATIVE("development_test_file.bin"));
+		game_state.load_scenario_data();
+		game_state.local_player_nation = dcon::nation_id{8};
+		game_state.world.nation_set_is_player_controlled(game_state.local_player_nation, true);
+		sys::write_scenario_file(game_state, NATIVE("development_test_file.bin"));
 	} else {
-		game_state->fill_unsaved_data();
+		game_state.fill_unsaved_data();
 	}
 
 	// scenario loading functions (would have to run these even when scenario is pre-built
-	game_state->load_user_settings();
-	text::load_standard_fonts(*game_state);
-	text::load_bmfonts(*game_state);
-	ui::populate_definitions_map(*game_state);
+	game_state.load_user_settings();
+	text::load_standard_fonts(game_state);
+	text::load_bmfonts(game_state);
+	ui::populate_definitions_map(game_state);
 
-	std::thread update_thread([&]() { game_state->game_loop(); });
+	std::thread update_thread([&]() { game_state.game_loop(); });
 
-	window::create_window(*game_state, window::creation_parameters());
+	window::create_window(game_state, window::creation_parameters());
 
-	game_state->quit_signaled.store(true, std::memory_order_release);
+	game_state.quit_signaled.store(true, std::memory_order_release);
 	update_thread.join();
 
 	return EXIT_SUCCESS;
