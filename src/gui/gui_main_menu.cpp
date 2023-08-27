cd2ce803bf8545d9d0d7c225075ace6ced5babdf
yrrrmmbl <n0op@protonmail.com>
2023-08-27 23:51:28 +0200

    pull request for schomby code review

--- src/gui/gui_main_menu.cpp
+++ src/gui/gui_main_menu.cpp
@@ -113,6 +113,24 @@ void autosave_display::on_update(sys::state& state) noexcept {
 			break;
 	}
 }
+
+
+void tooltip_mode_left::button_action(sys::state& state) noexcept {
+	state.user_settings.bind_tooltip_mouse = !state.user_settings.bind_tooltip_mouse;
+}
+void tooltip_mode_left::on_update(sys::state& state) noexcept {
+	send(state, parent, notify_setting_update{});
+}
+void tooltip_mode_right::button_action(sys::state& state) noexcept {
+	state.user_settings.bind_tooltip_mouse = !state.user_settings.bind_tooltip_mouse;
+}
+void tooltip_mode_right::on_update(sys::state& state) noexcept {
+	send(state, parent, notify_setting_update{});
+}
+void tooltip_mode_display::on_update(sys::state& state) noexcept {
+	if (state.user_settings.bind_tooltip_mouse)set_text(state, "Stick to Mouse");
+	else set_text(state, "Center over UI Element");
+}
 /*
 class autosave_left : public button_element_base {
 public:
