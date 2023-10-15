cc085088d36183d863b8c90d65786d35323742eb
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-10-15 20:17:02 +0100

    fix reverse scroll from mouse

--- src/gui/gui_element_types.cpp
+++ src/gui/gui_element_types.cpp
@@ -1186,6 +1186,7 @@ void listbox_element_base<RowWinT, RowConT>::update(sys::state& state) {
 template<class RowWinT, class RowConT>
 message_result listbox_element_base<RowWinT, RowConT>::on_scroll(sys::state& state, int32_t x, int32_t y, float amount, sys::key_modifiers mods) noexcept {
 	if(row_contents.size() > row_windows.size()) {
+		amount = is_reversed() ? amount : -amount;
 		list_scrollbar->update_raw_value(state, list_scrollbar->raw_value() + (amount < 0 ? 1 : -1));
 		update(state);
 	}
@@ -1195,6 +1196,7 @@ message_result listbox_element_base<RowWinT, RowConT>::on_scroll(sys::state& sta
 template<typename contents_type>
 message_result listbox2_base<contents_type>::on_scroll(sys::state& state, int32_t x, int32_t y, float amount, sys::key_modifiers mods) noexcept {
 	if(int32_t(row_contents.size()) > visible_row_count) {
+		amount = is_reversed() ? amount : -amount;
 		list_scrollbar->update_raw_value(state, list_scrollbar->raw_value() + (amount < 0 ? 1 : -1));
 		impl_on_update(state);
 	}
