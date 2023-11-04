3062434732978b2a0e74800aefc341d9a742ecf3
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-11-04 21:59:08 +0000

    nudge 5

--- src/gui/topbar_subwindows/gui_diplomacy_window.hpp
+++ src/gui/topbar_subwindows/gui_diplomacy_window.hpp
@@ -1294,11 +1294,11 @@ public:
 			country_relation = ptr.get();
 			auto drel_btn = make_element_by_type<diplomacy_action_decrease_relations_button>(state, "alice_plus_minus");
 			drel_btn->base_data.position = ptr->base_data.position; // nudge
+			drel_btn->base_data.position.x -= drel_btn->base_data.size.x; // nudge
 			drel_btn->base_data.position.y += drel_btn->base_data.size.y; // nudge
 			add_child_to_front(std::move(drel_btn));
 			auto irel_btn = make_element_by_type<diplomacy_action_increase_relations_button>(state, "alice_plus_minus");
 			irel_btn->base_data.position = ptr->base_data.position; // nudge
-			irel_btn->base_data.position.x += irel_btn->base_data.size.x; // nudge
 			irel_btn->base_data.position.y += irel_btn->base_data.size.y; // nudge
 			add_child_to_front(std::move(irel_btn));
 			return ptr;
