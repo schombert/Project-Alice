d6c29c88e6dd9fe32ed2b08673e64308949fcdfc
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-11-06 03:17:38 +0000

    reorder buttons

--- src/gui/topbar_subwindows/gui_diplomacy_window.hpp
+++ src/gui/topbar_subwindows/gui_diplomacy_window.hpp
@@ -2342,18 +2342,18 @@ public:
 		xy_pair options_offset = options_base_offset;
 		add_action_button<diplomacy_action_window<diplomacy_action_ally_button>>(state, options_offset);
 		options_offset.y += options_size.y;
-		add_action_button<diplomacy_action_window<diplomacy_action_call_ally_button>>(state, options_offset);
-		options_offset.y += options_size.y;
-		add_action_button<diplomacy_action_window<diplomacy_action_discredit_button>>(state, options_offset);
+		add_action_button<diplomacy_action_window<diplomacy_action_ban_embassy_button>>(state, options_offset);
 		options_offset.y += options_size.y;
 		add_action_button<diplomacy_action_window<diplomacy_action_expel_advisors_button>>(state, options_offset);
 		options_offset.y += options_size.y;
-		add_action_button<diplomacy_action_window<diplomacy_action_ban_embassy_button>>(state, options_offset);
+		add_action_button<diplomacy_action_window<diplomacy_action_discredit_button>>(state, options_offset);
 		options_offset.y += options_size.y;
 		add_action_button<diplomacy_action_window<diplomacy_action_release_subject_button>>(state, options_offset);
 		// Next row of actions...
 		options_offset.x += options_size.x;
 		options_offset.y = options_base_offset.y;
+		add_action_button<diplomacy_action_window<diplomacy_action_call_ally_button>>(state, options_offset);
+		options_offset.y += options_size.y;
 		add_action_button<diplomacy_action_window<diplomacy_action_military_access_button>>(state, options_offset);
 		options_offset.y += options_size.y;
 		add_action_button<diplomacy_action_window<diplomacy_action_give_military_access_button>>(state, options_offset);
