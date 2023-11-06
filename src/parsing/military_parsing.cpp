2ba82c948fa67f976205c5308ad8d545c3221729
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-11-06 23:21:55 +0000

    Better error reporting (giving file name)

--- src/parsing/military_parsing.cpp
+++ src/parsing/military_parsing.cpp
@@ -162,12 +162,30 @@ void make_oob_army(token_generator& gen, error_handler& err, oob_file_context& c
 	context.outer_context.state.world.force_create_army_control(id, context.nation_for);
 	oob_file_army_context new_context{context.outer_context, id, context.nation_for};
 	parse_oob_army(gen, err, new_context);
+
+	// and check they have correct unit types
+	auto p = context.outer_context.state.world.army_get_location_from_army_location(id);
+	for(auto m : context.outer_context.state.world.army_get_army_membership(id)) {
+		if(!bool(m.get_regiment().get_type())) {
+			auto name = context.outer_context.state.world.province_get_name(p);
+			err.accumulated_errors += "Army defined in " + text::produce_simple_string(context.outer_context.state, name) + " has a regiment, that does not have a valid type (" + err.file_name + ")\n";
+		}
+	}
 }
 void make_oob_navy(token_generator& gen, error_handler& err, oob_file_context& context) {
 	auto id = context.outer_context.state.world.create_navy();
 	context.outer_context.state.world.force_create_navy_control(id, context.nation_for);
 	oob_file_navy_context new_context{context.outer_context, id, context.nation_for};
 	parse_oob_navy(gen, err, new_context);
+
+	// and check they have correct unit types
+	auto p = context.outer_context.state.world.navy_get_location_from_navy_location(id);
+	for(auto m : context.outer_context.state.world.navy_get_navy_membership(id)) {
+		if(!bool(m.get_regiment().get_type())) {
+			auto name = context.outer_context.state.world.province_get_name(p);
+			err.accumulated_errors += "Navy defined in " + text::produce_simple_string(context.outer_context.state, name) + " has a ship, that does not have a valid type (" + err.file_name + ")\n";
+		}
+	}
 }
 void make_oob_regiment(token_generator& gen, error_handler& err, oob_file_army_context& context) {
 	auto id = context.outer_context.state.world.create_regiment();
