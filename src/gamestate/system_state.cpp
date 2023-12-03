c1deff6216d8122c39f0c0f4a4cb564c2604abbb
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-12-03 21:15:59 +0000

    conservative ideology fallback with name

--- src/gamestate/system_state.cpp
+++ src/gamestate/system_state.cpp
@@ -2725,8 +2725,14 @@ void state::load_scenario_data(parsers::error_handler& err) {
 		}
 	}
 	if(!culture_definitions.conservative) {
-		err.accumulated_errors += "NO CONSERVATIVE IDEOLOGY (fatal error)\n";
-		err.fatal = true;
+		if(auto it = context.map_of_ideologies.find("conservative"); it != context.map_of_ideologies.end()) {
+			culture_definitions.conservative = it->second.id;
+			err.accumulated_warnings += "conservative ideology will not reduce militancy\n";
+		}
+		if(!culture_definitions.conservative) {
+			err.accumulated_errors += "NO CONSERVATIVE IDEOLOGY (fatal error)\n";
+			err.fatal = true;
+		}
 	}
 	// triggered modifier contents
 	{
