0317852260091d78988c52663ae0fceaee25bccb
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-12-09 19:05:19 +0000

    map wioth better font

--- assets/shaders/text_line_f.glsl
+++ assets/shaders/text_line_f.glsl
@@ -14,7 +14,8 @@ vec4 gamma_correct(vec4 colour) {
 
 void main() {
 	float border_size = 0.05;
-	vec3 inner_color = vec3(1.0, 1.0, 1.0);
+	vec3 inner_color = vec3(0.0, 0.0, 0.0);
+	vec3 outer_color = vec3(1.0, 1.0, 1.0);
 	vec4 color_in = texture(texture0, tex_coord);
 	if(type == 0.f) {
 		color_in = texture(texture0, tex_coord);
@@ -27,17 +28,12 @@ void main() {
 	}
 	
 	if(color_in.r > 0.505) {
-		frag_color = vec4(inner_color, 1.0);
+		frag_color = vec4(inner_color, 1.0f);
 	} else if(color_in.r > 0.50) {
-		frag_color = vec4(mix(inner_color, vec3(1.0, 1.0, 1.0) - inner_color,  1.0f - (color_in.r - 0.5f) * 200.0f), 1.0);
+		frag_color = vec4(mix(inner_color, outer_color - inner_color,  1.0f - (color_in.r - 0.5f) * 200.0f), 1.0f);
 	} else {
 		float t = max(0.0f, color_in.r * 16.0f - 7.0f);
-		frag_color = vec4(vec3(1.0, 1.0, 1.0) - inner_color, t * t);
+		frag_color = vec4(outer_color - inner_color, t * t * t);
 	}
-	//} else {
-	//	float sm_val = smoothstep(0.5 - border_size * 1.5, 0.5 - border_size, color_in.r);
-	//	sm_val = min(sm_val, 0.5);
-	//	frag_color = vec4(vec3(0.0, 0.0, 0.0) - inner_color, sm_val);
-	//}
 	frag_color = gamma_correct(frag_color);
 }
