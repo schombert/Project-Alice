c3c832b2121611af7b898e83ab10a86ec2ef5153
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2023-12-11 08:14:39 +0000

    States use capital name as fallback

--- src/text/text.cpp
+++ src/text/text.cpp
@@ -841,6 +841,8 @@ std::string get_dynamic_state_name(sys::state const& state, dcon::state_instance
 			return adj + " " + get_name_as_string(state, fat_id.get_definition());
 		}
 	}
+	if(!fat_id.get_definition().get_name())
+		return get_name_as_string(state, fat_id.get_capital());
 	return get_name_as_string(state, fat_id.get_definition());
 }
 
@@ -850,7 +852,10 @@ std::string get_province_state_name(sys::state const& state, dcon::province_id p
 	if(state_instance_id) {
 		return get_dynamic_state_name(state, state_instance_id);
 	} else {
-		return get_name_as_string(state, fat_id.get_abstract_state_membership_as_province().get_state());
+		auto sdef = fat_id.get_abstract_state_membership_as_province().get_state();
+		if(!sdef.get_name())
+			return get_name_as_string(state, fat_id.get_state_membership().get_capital());
+		return get_name_as_string(state, sdef);
 	}
 }
 
