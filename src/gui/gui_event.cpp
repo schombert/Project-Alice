4967721516162feb91ab670883fea0e82cf97774
wxwisiasdf <jesusantonio30122016@gmail.com>
2023-12-18 03:09:38 +0000

    Update gui_event.cpp

--- src/gui/gui_event.cpp
+++ src/gui/gui_event.cpp
@@ -414,8 +414,8 @@ void event_desc_text::on_update(sys::state& state) noexcept {
 		if(imm) {
 			effect_description(state, contents, imm, phe.primary_slot, trigger::to_generic(phe.n), phe.from_slot, phe.r_lo, phe.r_hi);
 			for(auto& l : delegate->internal_layout.contents) {
-				if(l.color == text::text_color::white)
-					l.color = text::text_color::black;
+				if(l.color == delegate->black_text ? text::text_color::white : text::text_color::black) //Invert colours
+					l.color = delegate->black_text ? text::text_color::black : text::text_color::white;
 			}
 		}
 	} else if(std::holds_alternative<event::pending_human_f_n_event>(content)) {
@@ -424,8 +424,8 @@ void event_desc_text::on_update(sys::state& state) noexcept {
 		if(imm) {
 			effect_description(state, contents, imm, trigger::to_generic(phe.n), trigger::to_generic(phe.n), -1, phe.r_lo, phe.r_hi);
 			for(auto& l : delegate->internal_layout.contents) {
-				if(l.color == text::text_color::white)
-					l.color = text::text_color::black;
+				if(l.color == delegate->black_text ? text::text_color::white : text::text_color::black) //Invert colours
+					l.color = delegate->black_text ? text::text_color::black : text::text_color::white;
 			}
 		}
 	}
