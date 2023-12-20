c0c95e35f8303f4c726ba795b290541f461fd460
OptimizedForDensity <105244635+OptimizedForDensity@users.noreply.github.com>
2023-12-19 19:15:22 -0500

    Add religious conversions

--- src/economy/demographics.cpp
+++ src/economy/demographics.cpp
@@ -1557,7 +1557,7 @@ void update_assimilation(sys::state& state, uint32_t offset, uint32_t divisions,
 					*/
 
 					auto pc = state.world.pop_get_culture(p);
-					
+
 					if(!state.world.culture_group_get_is_overseas(state.world.culture_get_group_from_culture_group_membership(pc))) {
 						base_amount /= 10.0f;
 					}
@@ -1622,7 +1622,7 @@ float get_estimated_assimilation(sys::state& state, dcon::pop_id ids) {
 	if(!state.world.culture_group_get_is_overseas(state.world.culture_get_group_from_culture_group_membership(pc))) {
 		base_amount /= 10.0f;
 	}
-	
+
 
 	/*
 	All pops have their assimilation numbers reduced by a factor of 100 per core in the province sharing their primary
@@ -1642,6 +1642,88 @@ float get_estimated_assimilation(sys::state& state, dcon::pop_id ids) {
 	/*if(current_size < 100.0f && base_amount >= 0.001f) {
 		return current_size;
 	} else*/ if(base_amount >= 0.001f) {
+		return std::min(current_size, std::ceil(base_amount));
+	} else {
+		return 0.0f;
+	}
+}
+
+void update_conversion(sys::state& state, uint32_t offset, uint32_t divisions, conversion_buffer& pbuf) {
+	pbuf.update(state.world.pop_size());
+
+	/*
+	- religious conversion -- Conversion is per-month rather than per-day as in Victoria 2.
+	*/
+
+	pexecute_staggered_blocks(offset, divisions, state.world.pop_size(), [&](auto ids) {
+		pbuf.amounts.set(ids, 0.0f);
+		auto loc = state.world.pop_get_province_from_pop_location(ids);
+		auto owners = state.world.province_get_nation_from_province_ownership(loc);
+		auto conversion_chances = ve::max(trigger::evaluate_additive_modifier(state, state.culture_definitions.conversion_chance, trigger::to_generic(ids), trigger::to_generic(ids), 0), 0.0f);
+
+		ve::apply(
+				[&](dcon::pop_id p, dcon::province_id location, dcon::nation_id owner, float conversion_chance) {
+					// no conversion in unowned provinces
+					if(!owner)
+						return; // early exit
+
+					auto state_religion = state.world.nation_get_religion(owner);
+					// pops of the state religion do not convert
+					if(state_religion == state.world.pop_get_religion(p))
+						return; // early exit
+
+					// need at least 1 pop following the religion in the province
+					if(state.world.province_get_demographics(location, demographics::to_key(state, state_religion.id)) < 1.f)
+						return; // early exit
+
+					/*
+					Amount: define:CONVERSION_SCALE x (provincial-conversion-rate-modifier + 1) x
+					(national-conversion-rate-modifier + 1) x pop-size x conversion chance factor (computed additively, and always
+					at least 0.01).
+					*/
+
+					float current_size = state.world.pop_get_size(p);
+					float base_amount =
+							state.defines.conversion_scale *
+							std::max(0.0f, (state.world.province_get_modifier_values(location, sys::provincial_mod_offsets::conversion_rate) + 1.0f)) *
+							std::max(0.0f, (state.world.nation_get_modifier_values(owner, sys::national_mod_offsets::global_conversion_rate) + 1.0f)) *
+							conversion_chance * current_size;
+
+					if(base_amount >= 0.001f) {
+						auto transfer_amount = std::min(current_size, std::ceil(base_amount));
+						pbuf.amounts.set(p, transfer_amount);
+					}
+				},
+				ids, loc, owners, conversion_chances);
+	});
+}
+
+float get_estimated_conversion(sys::state& state, dcon::pop_id ids) {
+	auto location = state.world.pop_get_province_from_pop_location(ids);
+	auto owner = state.world.province_get_nation_from_province_ownership(location);
+	auto conversion_chances = std::max(trigger::evaluate_additive_modifier(state, state.culture_definitions.conversion_chance, trigger::to_generic(ids), trigger::to_generic(ids), 0), 0.0f);
+
+	// pops of the state religion do not convert
+	if(state.world.nation_get_religion(owner) == state.world.pop_get_religion(ids))
+		return 0.0f; // early exit
+
+	auto state_religion = state.world.nation_get_religion(owner);
+	// pops of the state religion do not convert
+	if(state_religion == state.world.pop_get_religion(ids))
+		return 0.0f; // early exit
+
+	// need at least 1 pop following the religion in the province
+	if(state.world.province_get_demographics(location, demographics::to_key(state, state_religion.id)) < 1.f)
+		return 0.0f; // early exit
+
+	float current_size = state.world.pop_get_size(ids);
+	float base_amount =
+		state.defines.conversion_scale *
+		std::max(0.0f, (state.world.province_get_modifier_values(location, sys::provincial_mod_offsets::conversion_rate) + 1.0f)) *
+		std::max(0.0f, (state.world.nation_get_modifier_values(owner, sys::national_mod_offsets::global_conversion_rate) + 1.0f)) *
+			conversion_chances * current_size;
+
+	if(base_amount >= 0.001f) {
 		return std::min(current_size, std::ceil(base_amount));
 	} else {
 		return 0.0f;
@@ -1865,7 +1947,7 @@ float get_estimated_internal_migration(sys::state& state, dcon::pop_id ids) {
 	auto amount = std::max(trigger::evaluate_additive_modifier(state, state.culture_definitions.migration_chance,
 		 trigger::to_generic(ids), trigger::to_generic(ids), 0),  0.0f) * pop_sizes * std::max(0.0f, (state.world.province_get_modifier_values(loc, sys::provincial_mod_offsets::immigrant_push) + 1.0f)) * state.defines.immigration_scale;
 
-	
+
 	if(amount <= 0.0f)
 		return 0.0f; // early exit
 
@@ -1940,7 +2022,7 @@ float get_estimated_colonial_migration(sys::state& state, dcon::pop_id ids) {
 			pt == state.culture_definitions.secondary_factory_worker)
 		return 0.0f; // early exit
 
-	
+
 	auto pop_sizes = state.world.pop_get_size(ids);
 	auto amounts = std::max(trigger::evaluate_additive_modifier(state, state.culture_definitions.colonialmigration_chance,
 			trigger::to_generic(ids), trigger::to_generic(ids), 0),  0.0f)
@@ -1951,7 +2033,7 @@ float get_estimated_colonial_migration(sys::state& state, dcon::pop_id ids) {
 
 	if(amounts <= 0.0f)
 		return 0.0f; // early exit
-				
+
 	auto pop_size = state.world.pop_get_size(ids);
 	return std::min(pop_size, std::ceil(amounts));
 }
@@ -2217,6 +2299,27 @@ void apply_assimilation(sys::state& state, uint32_t offset, uint32_t divisions, 
 					}
 				},
 				ids, locs, state.world.province_get_dominant_accepted_culture(locs));
+	});
+}
+
+void apply_conversion(sys::state& state, uint32_t offset, uint32_t divisions, conversion_buffer& pbuf) {
+	execute_staggered_blocks(offset, divisions, std::min(state.world.pop_size(), pbuf.size), [&](auto ids) {
+		auto locs = state.world.pop_get_province_from_pop_location(ids);
+		ve::apply(
+				[&](dcon::pop_id p, dcon::province_id l) {
+					if(pbuf.amounts.get(p) > 0.0f) {
+						auto state_rel = state.world.nation_get_religion(nations::owner_of_pop(state, p));
+						auto rel = state_rel
+							? state_rel
+							: state.world.province_get_dominant_religion(l);
+						assert(state.world.pop_get_poptype(p));
+						assert(state.world.pop_get_culture(p));
+						auto target_pop = impl::find_or_make_pop(state, l, state.world.pop_get_culture(p), rel, state.world.pop_get_poptype(p));
+						state.world.pop_get_size(p) -= pbuf.amounts.get(p);
+						state.world.pop_get_size(target_pop) += pbuf.amounts.get(p);
+					}
+				},
+				ids, locs);
 	});
 }
 
