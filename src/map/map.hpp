729744fb1edd411c436de2511b18b8fc7fe3fa8f
schombert <schombert@users.noreply.github.com>
2023-12-20 20:35:15 -0800

    improve globe mesh

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -182,6 +182,8 @@ public:
 	GLuint coastal_vao = 0;
 	GLuint coastal_border_vbo = 0;
 
+	std::vector<uint16_t> map_indices;
+
 	// Textures
 	GLuint provinces_texture_handle = 0;
 	GLuint terrain_texture_handle = 0;
