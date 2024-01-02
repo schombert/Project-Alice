a1423c2619c25aca6545b540357af6cf075efa32
wxwisiasdf <jesusantonio30122016@gmail.com>
2024-01-02 07:16:12 +0000

    placement of 3d models on globe

--- assets/shaders/model3d_v.glsl
+++ assets/shaders/model3d_v.glsl
@@ -16,46 +16,46 @@ subroutine uniform calc_gl_position_class calc_gl_position;
 
 layout(index = 0) subroutine(calc_gl_position_class)
 vec4 globe_coords(vec3 world_pos) {
-	vec3 new_world_pos;
-	float section_x = 200;
-	float angle_x1 = 2 * PI * floor(world_pos.x * section_x) / section_x;
-	float angle_x2 = 2 * PI * floor(world_pos.x * section_x + 1) / section_x;
-	float angle_x = mix(angle_x1, angle_x2, mod(world_pos.x * section_x, 1));
-	new_world_pos.x = mix(cos(angle_x1), cos(angle_x2), mod(world_pos.x * section_x, 1));
-	new_world_pos.y = mix(sin(angle_x1), sin(angle_x2), mod(world_pos.x * section_x, 1));
-	float section_y = 200;
-	float angle_y1 = PI * floor(world_pos.y * section_y) / section_y;
-	float angle_y2 = PI * floor(world_pos.y * section_y + 1) / section_y;
-	new_world_pos.x *= mix(sin(angle_y1), sin(angle_y2), mod(world_pos.y * section_y, 1));
-	new_world_pos.y *= mix(sin(angle_y1), sin(angle_y2), mod(world_pos.y * section_y, 1));
-	new_world_pos.z = mix(cos(angle_y1), cos(angle_y2), mod(world_pos.y * section_y, 1));
-	new_world_pos = rotation * new_world_pos;
-	new_world_pos /= PI; 		// Will make the zoom be the same for the globe and flat map
-	new_world_pos.y *= 0.02; 	// Sqeeze the z coords. Needs to be between -1 and 1
-	new_world_pos.xz *= -1; 	// Invert the globe
-	new_world_pos.xyz += 0.5; 	// Move the globe to the center
+    vec3 new_world_pos;
+    float angle_x = 2 * world_pos.x * PI;
+    new_world_pos.x = cos(angle_x);
+    new_world_pos.y = sin(angle_x);
 
-	return vec4(
-		(2. * new_world_pos.x - 1.f) / aspect_ratio  * zoom,
-		(2. * new_world_pos.z - 1.f) * zoom,
-		(2. * new_world_pos.y - 1.f), 1.0);
+    float angle_y = world_pos.z * PI;
+    new_world_pos.x *= sin(angle_y);
+    new_world_pos.y *= sin(angle_y);
+    new_world_pos.z = cos(angle_y);
+    new_world_pos = rotation * new_world_pos;
+    new_world_pos /= PI;         // Will make the zoom be the same for the globe and flat map
+    new_world_pos *= (1 + world_pos.y);
+    new_world_pos.y *= 0.02;     // Sqeeze the z coords. Needs to be between -1 and 1
+    new_world_pos.xz *= -1;     // Invert the globe
+    new_world_pos.xyz += 0.5;     // Move the globe to the center
+    return vec4(
+        (2. * new_world_pos.x - 1.f) / aspect_ratio  * zoom,
+        (2. * new_world_pos.z - 1.f) * zoom,
+        (2. * new_world_pos.y - 1.f), 1.0);
 }
 
 layout(index = 1) subroutine(calc_gl_position_class)
 vec4 flat_coords(vec3 world_pos) {
-	float vertical_factor = ((map_size.x + map_size.y) / 2.f);
-	world_pos /= vec3(map_size.x, map_size.y, vertical_factor);
-	world_pos -= vec3(offset.x, 0.f, offset.y);
+	world_pos -= vec3(offset.x, 0.f, -offset.y);
 	world_pos.x = mod(world_pos.x, 1.0f);
 	return vec4(
 		(2.f * +world_pos.x - 1.f) * zoom,
-		(2.f * -world_pos.z - 1.f) * zoom,
-		(2.f * +world_pos.y),
+		(2.f * +world_pos.z - 1.f) * zoom,
+		(2.f * -world_pos.y),
 		1.0f
 	);
 }
 
 void main() {
-	gl_Position = calc_gl_position(vertex_position);
+	float vertical_factor = ((map_size.x + map_size.y) / 2.f);
+    float height_scale = map_size.x / 2.;
+	vec3 world_pos = vertex_position / vec3(map_size.x, height_scale, vertical_factor);
+    world_pos.z *= -1;
+    world_pos *= 10;
+    world_pos += vec3(0.5, 0, 0.5);
+	gl_Position = calc_gl_position(world_pos);
 	tex_coord = texture_coord;
 }
