ee4c80e87a0c8bd61092e7919673453f1fd5beba
aintDatCap <federico.caprini@protonmail.com>
2024-01-06 22:40:34 +0100

    Started GLSL -> HLSL conversions

---
+++ assets/shaders/hlsl/line_unit_arrow_pix.hlsl
@@ -0,0 +1,26 @@
+struct Input {
+	float2 text_coord;
+};
+
+struct Output {
+	float4 frag_color:SV_Target0; 
+};
+
+uniform Texture2D unit_arrow_texture:register(t0);
+uniform SamplerState unit_arrow_sampler:register(s0);
+
+uniform float gamma:SV_Target1;
+
+
+float4 gamma_correct(float4 color) {
+	return float4(pow(color.rgb, float3(1.f/gamma)), color.a);
+}
+
+
+Output main(Input input) {
+	Output output;
+	//frag_color = vec4(tex_coord.x, tex_coord.y, 0, 1.);
+	float4 out_color = unit_arrow_texture.Sample(unit_arrow_sampler, input.text_coord );
+	output.frag_color = gamma_correct(out_color);
+	return output;
+}
