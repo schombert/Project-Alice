5bc1938a1bb86fd13a4d3ae391ac3ea6299a2bfc
Masterchef118 <thelvadumee7162@gmail.com>
2024-01-13 05:40:37 -0500

    Added saving and loading of new setting.

--- src/gamestate/system_state.cpp
+++ src/gamestate/system_state.cpp
@@ -2031,6 +2031,7 @@ void state::save_user_settings() const {
 	US_SAVE(autosaves);
 	US_SAVE(bind_tooltip_mouse);
 	US_SAVE(use_classic_fonts);
+	US_SAVE(left_mouse_click_hold_and_release);
 	US_SAVE(outliner_views);
 	constexpr size_t lower_half_count = 98;
 	std::memcpy(ptr, user_settings.self_message_settings, lower_half_count);
@@ -2082,6 +2083,7 @@ void state::load_user_settings() {
 			US_LOAD(autosaves);
 			US_LOAD(bind_tooltip_mouse);
 			US_LOAD(use_classic_fonts);
+			US_LOAD(left_mouse_click_hold_and_release);
 			US_LOAD(outliner_views);
 			constexpr size_t lower_half_count = 98;
 
