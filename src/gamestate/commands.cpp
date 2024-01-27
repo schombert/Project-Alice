3827663e14f72ba8083a885f0421fc20dd60e0fc
wxwisiasdf <jesusantonio30122016@gmail.com>
2024-01-27 03:45:03 +0000

    Update commands.cpp

--- src/gamestate/commands.cpp
+++ src/gamestate/commands.cpp
@@ -4294,7 +4294,9 @@ bool can_notify_player_leaves(sys::state& state, dcon::nation_id source) {
 	return true;
 }
 void execute_notify_player_leaves(sys::state& state, dcon::nation_id source) {
-	//state.world.nation_set_is_player_controlled(source, false);
+	if(state.network_state.headless) { //only automatically set to AI on headless servers
+		state.world.nation_set_is_player_controlled(source, false);
+	}
 	ui::chat_message m{};
 	m.source = source;
 	text::substitution_map sub{};
