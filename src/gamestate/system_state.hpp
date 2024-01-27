c845dc4a2d392d449e05a88d4c0e082fe3bc8d5d
wxwisiasdf <39974089+wxwisiasdf@users.noreply.github.com>
2024-01-27 20:31:36 +0000

    Hide country_flags by default (unless user wants to be spoilered, via "show spoilers option)

--- src/gamestate/system_state.hpp
+++ src/gamestate/system_state.hpp
@@ -374,6 +374,7 @@ struct user_settings_s {
 	bool render_models = false;
 	bool mouse_edge_scrolling = false;
 	bool black_map_font = true;
+	bool spoilers = false;
 };
 
 struct global_scenario_data_s { // this struct holds miscellaneous global properties of the scenario
