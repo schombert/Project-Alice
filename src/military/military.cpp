359d1eab20156e76aeb12048c6961fe459a51ed9
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-03-03 01:29:28 +0000

    fix

--- src/military/military.cpp
+++ src/military/military.cpp
@@ -4831,28 +4831,31 @@ bool will_recieve_attrition(sys::state& state, dcon::army_id a) {
 	return total_army_weight * attrition_mod - (supply_limit + prov_attrition_mod + greatest_hostile_fort) > 0;
 }
 
-float relative_attrition_amount(sys::state& state, dcon::nation_id n, std::vector<dcon::army_id>& list, dcon::province_id prov) {
-	float total_army_weight = 0.f;
-	for(auto army : list) {
-		auto ar = dcon::fatten(state.world, army);
-		if(ar.get_black_flag() == false && ar.get_is_retreating() == false && !bool(ar.get_navy_from_army_transport())) {
-			for(auto rg : ar.get_army_membership()) {
-				total_army_weight += 3.0f * rg.get_regiment().get_strength();
-			}
-		}
-	}
+float relative_attrition_amount(sys::state& state, dcon::navy_id a, dcon::province_id prov) {
+	return 0.0f;
+}
+float relative_attrition_amount(sys::state& state, dcon::army_id a, dcon::province_id prov) {
+	float total_army_weight = 0;
 	for(auto ar : state.world.province_get_army_location(prov)) {
-		if(ar.get_army().get_black_flag() == false && ar.get_army().get_is_retreating() == false && !bool(ar.get_army().get_navy_from_army_transport())) {
+		if(ar.get_army().get_black_flag() == false && ar.get_army().get_is_retreating() == false &&
+				!bool(ar.get_army().get_navy_from_army_transport())) {
+
 			for(auto rg : ar.get_army().get_army_membership()) {
 				total_army_weight += 3.0f * rg.get_regiment().get_strength();
 			}
 		}
 	}
+
 	auto prov_attrition_mod = state.world.province_get_modifier_values(prov, sys::provincial_mod_offsets::attrition);
-	auto army_controller = dcon::fatten(state.world, n);
+
+	auto ar = fatten(state.world, a);
+
+	auto army_controller = ar.get_controller_from_army_control();
 	auto supply_limit = supply_limit_in_province(state, army_controller, prov);
 	auto attrition_mod = 1.0f + army_controller.get_modifier_values(sys::national_mod_offsets::land_attrition);
+
 	float greatest_hostile_fort = 0.0f;
+
 	for(auto adj : state.world.province_get_province_adjacency(prov)) {
 		if((adj.get_type() & (province::border::impassible_bit | province::border::coastal_bit)) == 0) {
 			auto other = adj.get_connected_provinces(0) != prov ? adj.get_connected_provinces(0) : adj.get_connected_provinces(1);
@@ -4863,7 +4866,16 @@ float relative_attrition_amount(sys::state& state, dcon::nation_id n, std::vecto
 			}
 		}
 	}
-	return total_army_weight * attrition_mod - (supply_limit + prov_attrition_mod + greatest_hostile_fort) > 0;
+
+	auto value = std::clamp(total_army_weight * attrition_mod - (supply_limit + prov_attrition_mod + greatest_hostile_fort), 0.0f, state.world.province_get_modifier_values(prov, sys::provincial_mod_offsets::max_attrition))
+		+ state.world.province_get_siege_progress(prov) > 0.f ? state.defines.siege_attrition : 0.0f;
+	return value * 0.01f;
+}
+float attrition_amount(sys::state& state, dcon::navy_id a) {
+	return relative_attrition_amount(state, a, state.world.navy_get_location_from_navy_location(a));
+}
+float attrition_amount(sys::state& state, dcon::army_id a) {
+	return relative_attrition_amount(state, a, state.world.army_get_location_from_army_location(a));
 }
 
 void apply_attrition(sys::state& state) {
--- src/military/military.hpp
+++ src/military/military.hpp
@@ -414,7 +414,10 @@ bool will_recieve_attrition(sys::state& state, dcon::navy_id a);
 bool will_recieve_attrition(sys::state& state, dcon::army_id a);
 float attrition_amount(sys::state& state, dcon::navy_id a);
 float attrition_amount(sys::state& state, dcon::army_id a);
-float relative_attrition_amount(sys::state& state, dcon::nation_id n, std::vector<dcon::army_id>& a, dcon::province_id prov);
+float relative_attrition_amount(sys::state& state, dcon::navy_id a, dcon::province_id prov);
+float relative_attrition_amount(sys::state& state, dcon::army_id a, dcon::province_id prov);
+float attrition_amount(sys::state& state, dcon::navy_id a);
+float attrition_amount(sys::state& state, dcon::army_id a);
 float peacetime_attrition_limit(sys::state& state, dcon::nation_id n, dcon::province_id prov);
 float reinforce_amount(sys::state& state, dcon::army_id a);
 
