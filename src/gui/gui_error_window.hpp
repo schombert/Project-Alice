63fe74175a3600be858ac0d79803b5e0a7122f10
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-04-05 04:23:42 +0100

    fix

--- src/gui/gui_error_window.hpp
+++ src/gui/gui_error_window.hpp
@@ -4,13 +4,22 @@
 #include "gui_element_types.hpp"
 
 namespace ui {
-class error_body_text : public simple_multiline_body_text {
+class error_body_text : public scrollable_text {
 public:
 	std::string msg = "";
 	void populate_layout(sys::state& state, text::endless_layout& contents) noexcept override {
 		auto box = text::open_layout_box(contents);
 		text::add_to_layout_box(state, contents, box, msg, text::text_color::white);
 		text::close_layout_box(contents, box);
+	}
+	void on_update(sys::state& state) noexcept override {
+		auto container = text::create_endless_layout(delegate->internal_layout,
+			text::layout_parameters{ 0, 0, static_cast<int16_t>(base_data.size.x), static_cast<int16_t>(base_data.size.y),
+				base_data.data.text.font_handle, 0, text::alignment::left,
+				text::is_black_from_font_id(base_data.data.text.font_handle) ? text::text_color::black : text::text_color::white,
+				false });
+		populate_layout(state, container);
+		calibrate_scrollbar(state);
 	}
 };
 class error_dialog_window : public window_element_base {
