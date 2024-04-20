b2f68b78925337f8665f1bc87a460d7fa9cc67e6
Nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-04-20 07:04:27 +0100

    Update gui_nation_picker.hpp

--- src/gui/gui_nation_picker.hpp
+++ src/gui/gui_nation_picker.hpp
@@ -586,7 +586,6 @@ public:
 			//clients cant start the game, only tell that they're "ready"
 		} else {
 			command::notify_start_game(state, state.local_player_nation);
-			state.map_state.map_data.set_selected_province(state, dcon::province_id{});
 		}
 	}
 
