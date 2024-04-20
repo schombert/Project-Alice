19ac8bbba54928a083ffb0b5ef93e1707662d1f2
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-04-20 05:39:11 +0100

    fx

--- src/gui/topbar_subwindows/gui_budget_window.hpp
+++ src/gui/topbar_subwindows/gui_budget_window.hpp
@@ -246,15 +246,15 @@ public:
 			datapoints[i] = value;
 		}
 
+		float area_ratio = integral / (total_area + 0.0001f);
 		if(state.user_settings.color_blind_mode == sys::color_blind_mode::none) {
-			float area_ratio = integral / (total_area + 0.0001f);
 			r = 1.f - area_ratio * 0.5f;
 			g = std::sqrt(area_ratio);
 			b = std::sqrt(area_ratio) * 0.8f;
 		} else {
-			r = 0.f;
-			g = 0.f;
-			b = 0.f;
+			r = area_ratio;
+			g = area_ratio;
+			b = area_ratio;
 		}
 		set_data_points(state, datapoints, min, max);
 	}
