6a59c036713d84afd671ca6ede4fc7086032592e
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-04-21 22:57:00 +0100

    Fix OOB parsing on TNO

--- src/gamestate/system_state.cpp
+++ src/gamestate/system_state.cpp
@@ -2773,9 +2773,8 @@ void state::load_scenario_data(parsers::error_handler& err) {
 					break;
 				}
 			}
-			if(last - start_of_name >= 6 && file_name.ends_with(NATIVE("_oob.txt"))) {
+			if(last - start_of_name >= 3) {
 				auto utf8name = simple_fs::native_to_utf8(native_string_view(start_of_name, last - start_of_name));
-
 				if(auto it = context.map_of_ident_names.find(nations::tag_to_int(utf8name[0], utf8name[1], utf8name[2]));
 						it != context.map_of_ident_names.end()) {
 					auto holder = context.state.world.national_identity_get_nation_from_identity_holder(it->second);
