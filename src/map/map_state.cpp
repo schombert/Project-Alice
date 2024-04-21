d8790250d9e88052822fc2481b558e9cc6e3cdb0
Nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-04-21 07:23:30 +0100

    Update map_state.cpp

--- src/map/map_state.cpp
+++ src/map/map_state.cpp
@@ -133,7 +133,7 @@ void update_text_lines(sys::state& state, display_data& map_data) {
 					}
 				}
 			}
-			for(const auto e : map) {
+			for(const auto& e : map) {
 				if(float(e.second) / float(total_provinces) >= 0.75f) {
 					// Adjective + " " + National identity
 					auto const nid = dcon::national_identity_id(dcon::national_identity_id::value_base_t(e.first));
