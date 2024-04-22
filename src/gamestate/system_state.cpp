11c26d09924ff2556e571687db2c9101a3f5dfb6
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-04-22 01:54:45 +0100

    fix clicking on nation with mouse hold

--- src/gamestate/system_state.cpp
+++ src/gamestate/system_state.cpp
@@ -202,8 +202,6 @@ void state::on_lbutton_up(int32_t x, int32_t y, key_modifiers mod) {
 			} else if(ui_state.under_mouse != ui_state.left_mouse_hold_target) {
 				ui_state.left_mouse_hold_target->impl_on_lbutton_up(*this, ui_state.relative_mouse_location.x, ui_state.relative_mouse_location.y, mod, false);
 			}
-			map_state.on_lbutton_up(*this, x, y, x_size, y_size, mod);
-			return;
 		} else {
 			if(ui_state.under_mouse == ui_state.left_mouse_hold_target) {
 				ui_state.left_mouse_hold_target = nullptr;
@@ -240,6 +238,10 @@ void state::on_lbutton_up(int32_t x, int32_t y, key_modifiers mod) {
 	} else if(mode == sys::game_mode_type::select_states) {
 		auto sdef = world.province_get_state_from_abstract_state_membership(map_state.selected_province);
 		state_select(sdef);
+	}
+
+	if(user_settings.left_mouse_click_hold_and_release && ui_state.left_mouse_hold_target && mode != sys::game_mode_type::in_game) {
+		return;
 	}
 
 	ui_state.scrollbar_timer = 0;
