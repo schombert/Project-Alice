c214c172d66550c03e9f3518724d0861de0c90c0
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-05-02 06:38:18 +0100

    fix button sfx

--- src/gui/gui_element_types.hpp
+++ src/gui/gui_element_types.hpp
@@ -240,7 +240,7 @@ public:
 
 	message_result on_lbutton_down(sys::state& state, int32_t x, int32_t y, sys::key_modifiers mods) noexcept override {
 		if(!state.user_settings.left_mouse_click_hold_and_release && !disabled) {
-			sound::play_interface_sound(state, sound::get_click_sound(state),
+			sound::play_interface_sound(state, get_click_sound(state),
 					state.user_settings.interface_volume * state.user_settings.master_volume);
 			if(mods == sys::key_modifiers::modifiers_shift)
 				button_shift_action(state);
@@ -253,7 +253,7 @@ public:
 	}
 	message_result on_rbutton_down(sys::state& state, int32_t x, int32_t y, sys::key_modifiers mods) noexcept override {
 		if(!disabled) {
-			sound::play_interface_sound(state, sound::get_click_sound(state), state.user_settings.interface_volume * state.user_settings.master_volume);
+			sound::play_interface_sound(state, get_click_sound(state), state.user_settings.interface_volume * state.user_settings.master_volume);
 			if(mods == sys::key_modifiers::modifiers_shift)
 				button_shift_right_action(state);
 			else if(mods == sys::key_modifiers::modifiers_ctrl)
@@ -265,7 +265,7 @@ public:
 	}
 	message_result on_lbutton_up(sys::state& state, int32_t x, int32_t y, sys::key_modifiers mods, bool under_mouse) noexcept override {
 		if(state.user_settings.left_mouse_click_hold_and_release && !disabled && under_mouse) {
-			sound::play_interface_sound(state, sound::get_click_sound(state),
+			sound::play_interface_sound(state, get_click_sound(state),
 				state.user_settings.interface_volume * state.user_settings.master_volume);
 			if(mods == sys::key_modifiers::modifiers_shift)
 				button_shift_action(state);
@@ -278,7 +278,7 @@ public:
 	}
 	message_result on_key_down(sys::state& state, sys::virtual_key key, sys::key_modifiers mods) noexcept final {
 		if(!disabled && base_data.get_element_type() == element_type::button && base_data.data.button.shortcut == key) {
-			sound::play_interface_sound(state, sound::get_click_sound(state),
+			sound::play_interface_sound(state, get_click_sound(state),
 					state.user_settings.interface_volume * state.user_settings.master_volume);
 			if(mods == sys::key_modifiers::modifiers_shift)
 				button_shift_action(state);
