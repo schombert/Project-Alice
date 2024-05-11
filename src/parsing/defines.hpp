2164cd80781d45a4353382ea4e632b805f9b9c90
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-05-11 20:05:34 +0100

    Make economy "easier" to manage by default

--- src/parsing/defines.hpp
+++ src/parsing/defines.hpp
@@ -657,14 +657,14 @@
 	LUA_DEFINES_LIST_ELEMENT(alice_full_reinforce, 1.000000)                                                             \
 	LUA_DEFINES_LIST_ELEMENT(alice_ai_offensive_strength_overestimate, 1.000000) \
 	LUA_DEFINES_LIST_ELEMENT(alice_military_score_leadership_factor, 1.000000) \
-	LUA_DEFINES_LIST_ELEMENT(alice_lf_needs_scale, 0.950000) \
-	LUA_DEFINES_LIST_ELEMENT(alice_ev_needs_scale, 0.950000) \
-	LUA_DEFINES_LIST_ELEMENT(alice_lx_needs_scale, 0.950000) \
+	LUA_DEFINES_LIST_ELEMENT(alice_lf_needs_scale, 0.000950) \
+	LUA_DEFINES_LIST_ELEMENT(alice_ev_needs_scale, 0.000950) \
+	LUA_DEFINES_LIST_ELEMENT(alice_lx_needs_scale, 0.000950) \
 	LUA_DEFINES_LIST_ELEMENT(alice_max_event_iterations, 8.000000) \
 	LUA_DEFINES_LIST_ELEMENT(alice_needs_scaling_factor, 100000.000000) \
 	LUA_DEFINES_LIST_ELEMENT(alice_factory_per_level_employment, 10000.0) \
 	LUA_DEFINES_LIST_ELEMENT(alice_domestic_investment_multiplier, 2.0) \
-	LUA_DEFINES_LIST_ELEMENT(alice_rgo_boost, 1.0) \
+	LUA_DEFINES_LIST_ELEMENT(alice_rgo_boost, 2.5) \
 	LUA_DEFINES_LIST_ELEMENT(alice_inputs_base_factor_artisans, 1.1) \
 	LUA_DEFINES_LIST_ELEMENT(alice_output_base_factor_artisans, 0.6) \
 	LUA_DEFINES_LIST_ELEMENT(alice_inputs_base_factor, 1.0) \
