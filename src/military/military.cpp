420e70bf7119b5d450899fbdc17fd9a3d67830c9
Nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-05-16 21:37:16 +0100

    Update military.cpp

--- src/military/military.cpp
+++ src/military/military.cpp
@@ -2840,7 +2840,7 @@ void implement_war_goal(sys::state& state, dcon::war_id war, dcon::cb_type_id wa
 		// No defined wargoal_t leads to defaulting to the from nation
 		dcon::nation_id holder = from;
 		if(wargoal_t) {
-			auto holder = state.world.national_identity_get_nation_from_identity_holder(wargoal_t);
+			holder = state.world.national_identity_get_nation_from_identity_holder(wargoal_t);
 			if(!holder) {
 				holder = state.world.create_nation();
 				state.world.nation_set_identity_from_identity_holder(holder, wargoal_t);
