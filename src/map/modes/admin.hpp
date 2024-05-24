ffb8f7c1d77e086221f5a13d32a115cf5db16d27
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-05-24 19:38:51 +0100

    Make Admin mapmode see other nations too, and RGO mapmode see all RGOs and their level of output

--- src/map/modes/admin.hpp
+++ src/map/modes/admin.hpp
@@ -5,21 +5,24 @@ std::vector<uint32_t> admin_map_from(sys::state& state) {
 	uint32_t texture_size = province_size + 256 - province_size % 256;
 
 	std::vector<uint32_t> prov_color(texture_size * 2);
-
+	dcon::province_id selected_province = state.map_state.get_selected_province();
+	dcon::nation_id selected_nation = selected_province
+		? state.world.province_get_nation_from_province_ownership(selected_province)
+		: state.local_player_nation;
 	state.world.for_each_province([&](dcon::province_id prov_id) {
+		auto i = province::to_map_id(prov_id);
 		auto fat_id = dcon::fatten(state.world, prov_id);
-		auto nation = fat_id.get_nation_from_province_ownership();
-
-		if(nation == state.local_player_nation) {
+		if(!selected_nation || fat_id.get_nation_from_province_ownership() == selected_nation) {
 			auto admin_efficiency = province::state_admin_efficiency(state, fat_id.get_state_membership());
-
-			uint32_t color = ogl::color_gradient(admin_efficiency, sys::pack_color(46, 247, 15), // red
-					sys::pack_color(247, 15, 15)																										 // green
+			uint32_t color = ogl::color_gradient(admin_efficiency,
+				sys::pack_color(46, 247, 15), // red
+				sys::pack_color(247, 15, 15) // green
 			);
-
-			auto i = province::to_map_id(prov_id);
 			prov_color[i] = color;
 			prov_color[i + texture_size] = color;
+		} else {
+			prov_color[i] = 0;
+			prov_color[i + texture_size] = 0;
 		}
 	});
 	return prov_color;
