cfdebada70a7fc321889c7f122eedde6e6c27dee
nivaturimika <159065448+Nivaturimika@users.noreply.github.com>
2024-06-09 22:52:48 +0100

    fix move cap

--- src/gui/gui_province_window.hpp
+++ src/gui/gui_province_window.hpp
@@ -531,6 +531,7 @@ public:
 			colony_button = ptr.get();
 			//...
 			auto btn = make_element_by_type<province_move_capital_button>(state, "alice_move_capital");
+			btn->base_data.position = colony_button->base_data.position;
 			btn->base_data.position.x -= colony_button->base_data.size.x * 2;
 			add_child_to_front(std::move(btn));
 			return ptr;
