8fa020dca14037d5524c9e9ca45986b556801cd6
Paul Nakonechnyy <sneakbug8@gmail.com>
2025-06-29 12:27:34 +0300

    Reorganize UI code

--- src/gui/labour_details.hpp
+++ src/gui/labour_details.hpp
@@ -1,8 +1,11 @@
+#pragma once
+
 namespace ui {
 
-// Return text key for tooltips on the provided labour type
-std::string inline labour_type_to_employment_type_text_key(int32_t type);
 // Return text key for labour name on the provided labour type
-std::string inline labour_type_to_employment_name_text_key(int32_t labor_type);
+std::string labour_type_to_employment_name_text_key(int32_t labor_type);
+// Return text key for tooltips on the provided labour type
+std::string labour_type_to_employment_type_text_key(int32_t type);
+
 
 }
