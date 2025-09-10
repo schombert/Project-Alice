b987e04e3fadfd03f0f8065d8fdb775bcf76df91
Paul Nakonechnyy <sneakbug8@gmail.com>
2025-09-10 10:47:43 +0300

    AI to build factories w/o a positive budget

--- src/ai/ai_economy.cpp
+++ src/ai/ai_economy.cpp
@@ -428,8 +428,8 @@ void update_ai_econ_construction(sys::state& state) {
 		float additional_expenses = 0.f;
 		float days_prepaid = 0.5f;
 		auto rules = n.get_combined_issue_rules();
-
-		if(budget < 0.f) {
+		// Try to build to expand the economy even without a positive budget (since spendings are scaled down)
+		if(budget < 0.f && state.world.nation_get_construction_spending(n) <= 0.1f) {
 			continue;
 		}
 
