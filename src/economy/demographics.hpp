3a0474c9f59f7efe3b2866116a87061c3f5f1fa5
ZombieFreak115 <knudsenmarius@gmail.com>
2025-11-03 14:54:56 +0100

    fix bug with type in promotion calculations. split logic into subfunctions, and make pop promotion Ui accurate

--- src/economy/demographics.hpp
+++ src/economy/demographics.hpp
@@ -250,6 +250,79 @@ enum class promotion_type : bool {
 	promotion = 1
 };
 
+struct promotion_demotion_weights {
+	tagged_vector<float, dcon::pop_type_id> pop_weights;
+	float total_weights;
+
+	promotion_demotion_weights(size_t size) {
+		pop_weights = tagged_vector<float, dcon::pop_type_id>(size);
+		total_weights = 0.0f;
+	}
+};
+template<promotion_type PROMOTION_TYPE>
+float get_single_promotion_demotion_target_weight(sys::state& state, dcon::pop_id p, dcon::pop_type_id target_type) {
+
+	auto ptype = state.world.pop_get_poptype(p);
+	auto strata = state.world.pop_type_get_strata(ptype);
+	auto loc = state.world.pop_get_province_from_pop_location(p);
+	auto si = state.world.province_get_state_membership(loc);
+	auto nf = state.world.state_instance_get_owner_focus(si);
+	auto promoted_type = state.world.national_focus_get_promotion_type(nf);
+	auto promotion_bonus = state.world.national_focus_get_promotion_amount(nf);
+
+	bool is_state_capital = state.world.state_instance_get_capital(state.world.province_get_state_membership(loc)) == loc;
+
+	if (target_type == ptype) {
+		return 0.0f; //don't promote to the same type
+	}
+	else if (!is_state_capital && state.world.pop_type_get_state_capital_only(target_type)) {
+		return 0.0f; //don't promote if the pop is not in the state capital
+	}
+	else if ((PROMOTION_TYPE == promotion_type::promotion && state.world.pop_type_get_strata(target_type) >= strata) //if the selected type is higher strata
+		|| (PROMOTION_TYPE == promotion_type::demotion && state.world.pop_type_get_strata(target_type) <= strata)) {   //if the selected type is lower strata
+
+		if (auto mfn = state.world.pop_type_get_promotion_fns(ptype, target_type); mfn != 0) {
+			using ftype = float(*)(int32_t);
+			ftype fn = (ftype)mfn;
+			float llvm_result = fn(p.index());
+#ifdef CHECK_LLVM_RESULTS
+			float interp_result = 0.0f;
+			if (auto mtrigger = state.world.pop_type_get_promotion(ptype, target_type); mtrigger) {
+				interp_result = trigger::evaluate_additive_modifier(state, mtrigger, trigger::to_generic(p), trigger::to_generic(p), 0);
+			}
+			assert(llvm_result == interp_result);
+#endif
+			auto chance = std::max(0.0f, llvm_result + (target_type == promoted_type ? promotion_bonus : 0.0f));
+			return chance;
+		}
+		else {
+			if (auto mtrigger = state.world.pop_type_get_promotion(ptype, target_type); mtrigger) {
+				auto chance = std::max(0.0f, trigger::evaluate_additive_modifier(state, mtrigger, trigger::to_generic(p), trigger::to_generic(p), 0) + (target_type == promoted_type ? promotion_bonus : 0.0f));
+				return chance;
+			}
+		}
+
+	}
+	return 0.0f;
+	
+}
+
+template<promotion_type PROMOTION_TYPE>
+promotion_demotion_weights get_pop_promotion_demotion_weights(sys::state& state, dcon::pop_id p) {
+
+	promotion_demotion_weights weights(state.world.pop_type_size());
+
+	state.world.for_each_pop_type([&](dcon::pop_type_id target_type) {
+
+		float weight = get_single_promotion_demotion_target_weight<PROMOTION_TYPE>(state, p, target_type);
+
+		weights.pop_weights[target_type] = weight;
+		weights.total_weights += weight;
+
+	});
+	return weights;
+}
+
 template<promotion_type PROMOTION_TYPE>
 pop_promotion_demotion_data get_promotion_demotion_data(sys::state& state, dcon::pop_id p, float promotion_or_demotion_chance) {
 	auto loc = state.world.pop_get_province_from_pop_location(p);
@@ -277,7 +350,6 @@ pop_promotion_demotion_data get_promotion_demotion_data(sys::state& state, dcon:
 		output_data.amount = transfer_amount;
 	}
 
-	tagged_vector<float, dcon::pop_type_id> weights(state.world.pop_type_size());
 
 	/*
 	The promotion and demotion factors seem to be computed additively (by taking the sum of all true conditions). If
@@ -294,77 +366,42 @@ pop_promotion_demotion_data get_promotion_demotion_data(sys::state& state, dcon:
 
 	bool is_state_capital = state.world.state_instance_get_capital(state.world.province_get_state_membership(loc)) == loc;
 
-	if((PROMOTION_TYPE == promotion_type::promotion && promoted_type && state.world.pop_type_get_strata(promoted_type) >= strata && (is_state_capital || state.world.pop_type_get_state_capital_only(promoted_type) == false))
+	if ((PROMOTION_TYPE == promotion_type::promotion && promoted_type && state.world.pop_type_get_strata(promoted_type) >= strata && (is_state_capital || state.world.pop_type_get_state_capital_only(promoted_type) == false))
 		|| (PROMOTION_TYPE == promotion_type::demotion && promoted_type && state.world.pop_type_get_strata(promoted_type) <= strata && (is_state_capital || state.world.pop_type_get_state_capital_only(promoted_type) == false))) {
 
 		float chance = 0.0f;
-		if(auto mfn = state.world.pop_type_get_promotion_fns(ptype, promoted_type); mfn != 0) {
+		if (auto mfn = state.world.pop_type_get_promotion_fns(ptype, promoted_type); mfn != 0) {
 			using ftype = float(*)(int32_t);
 			ftype fn = (ftype)mfn;
 			float llvm_result = fn(p.index());
 #ifdef CHECK_LLVM_RESULTS
 			float interp_result = 0.0f;
-			if(auto mtrigger = state.world.pop_type_get_promotion(ptype, promoted_type); mtrigger) {
+			if (auto mtrigger = state.world.pop_type_get_promotion(ptype, promoted_type); mtrigger) {
 				interp_result = trigger::evaluate_additive_modifier(state, mtrigger, trigger::to_generic(p), trigger::to_generic(p), 0);
 			}
 			assert(llvm_result == interp_result);
 #endif
 			chance = llvm_result + promotion_bonus;
-		} else {
-			if(auto mtrigger = state.world.pop_type_get_promotion(ptype, promoted_type); mtrigger) {
+		}
+		else {
+			if (auto mtrigger = state.world.pop_type_get_promotion(ptype, promoted_type); mtrigger) {
 				chance = trigger::evaluate_additive_modifier(state, mtrigger, trigger::to_generic(p), trigger::to_generic(p), 0) + promotion_bonus;
 			}
 		}
 
-		if(chance > 0) {
+		if (chance > 0) {
 			output_data.target = promoted_type;
 			return output_data; // early exit
 		}
+
 	}
+	promotion_demotion_weights weights = get_pop_promotion_demotion_weights<PROMOTION_TYPE>(state, p);
 
-	float chances_total = 0.0f;
-	state.world.for_each_pop_type([&](dcon::pop_type_id target_type) {
-		if(target_type == ptype) {
-			weights[target_type] = 0.0f; //don't promote to the same type
-		} else if(!is_state_capital && state.world.pop_type_get_state_capital_only(target_type)) {
-			weights[target_type] = 0.0f; //don't promote if the pop is not in the state capital
-		} else if((PROMOTION_TYPE == promotion_type::promotion && state.world.pop_type_get_strata(promoted_type) >= strata) //if the selected type is higher strata
-			|| (PROMOTION_TYPE == promotion_type::demotion && state.world.pop_type_get_strata(promoted_type) <= strata)) {   //if the selected type is lower strata
-
-			weights[target_type] = 0.0f;
-
-			if(auto mfn = state.world.pop_type_get_promotion_fns(ptype, target_type); mfn != 0) {
-				using ftype = float(*)(int32_t);
-				ftype fn = (ftype)mfn;
-				float llvm_result = fn(p.index());
-#ifdef CHECK_LLVM_RESULTS
-				float interp_result = 0.0f;
-				if(auto mtrigger = state.world.pop_type_get_promotion(ptype, target_type); mtrigger) {
-					interp_result = trigger::evaluate_additive_modifier(state, mtrigger, trigger::to_generic(p), trigger::to_generic(p), 0);
-				}
-				assert(llvm_result == interp_result);
-#endif
-				auto chance = llvm_result + (target_type == promoted_type ? promotion_bonus : 0.0f);
-				chances_total += chance;
-				weights[target_type] = chance;
-			} else {
-				if(auto mtrigger = state.world.pop_type_get_promotion(ptype, target_type); mtrigger) {
-					auto chance = trigger::evaluate_additive_modifier(state, mtrigger, trigger::to_generic(p), trigger::to_generic(p), 0) + (target_type == promoted_type ? promotion_bonus : 0.0f);
-					chances_total += chance;
-					weights[target_type] = chance;
-				}
-			}
-
-		} else {
-			weights[target_type] = 0.0f;
-		}
-	});
-
-	if(chances_total > 0.0f) {
+	if(weights.total_weights > 0.0f) {
 		auto rvalue = float(rng::get_random(state, uint32_t(p.index())) & 0xFFFF) / float(0xFFFF + 1);
 		for(uint32_t i = state.world.pop_type_size(); i-- > 0;) {
 			dcon::pop_type_id pr{ dcon::pop_type_id::value_base_t(i) };
-			rvalue -= weights[pr] / chances_total;
+			rvalue -= weights.pop_weights[pr] / weights.total_weights;
 			if(rvalue < 0.0f) {
 				output_data.target = pr;
 				return output_data;
@@ -376,6 +413,7 @@ pop_promotion_demotion_data get_promotion_demotion_data(sys::state& state, dcon:
 	return output_data;
 
 }
+
 
 
 struct promotion_buffer {
