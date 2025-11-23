342616284387a1f733994ad028173d4d005e6ae0
ZombieFreak115 <knudsenmarius@gmail.com>
2025-11-23 01:29:47 +0100

    another ugly fix

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -1083,7 +1083,15 @@ std::string add_compare_to_oos_report_indexed(const T& item_1, const T& item_2, 
 
 template<typename T>
 std::string add_compare_to_oos_report(const T& item_1, const T& item_2, const std::string& member_name) {
-	if((std::is_same<T, dcon::bitfield_type>::value && item_1.v != item_2.v) || item_1 != item_2) {
+	bool equal = false;
+	// have to have this special case as bitfield type does not have a comparison operator and is an external dependency
+	if constexpr(std::is_same<T, dcon::bitfield_type>::value) {
+		equal = (item_1.v == item_2.v);
+	}
+	else {
+		equal = (item_1 == item_2);
+	}
+	if(!equal) {
 		if constexpr(std::is_arithmetic<T>::value) {
 			return "\tobject " + member_name + ": " + std::to_string(item_1) + ", " + std::to_string(item_2) + "\n";
 		} else if constexpr(std::is_same<T, char>::value) {
