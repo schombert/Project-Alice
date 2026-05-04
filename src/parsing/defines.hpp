47d7da29592e00bfdd9076dd0b9bae1863647928
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2026-05-04 14:15:53 +0300

    fix extra spaces

--- src/parsing/defines.hpp
+++ src/parsing/defines.hpp
@@ -708,8 +708,8 @@
 	LUA_DEFINES_LIST_ELEMENT(alice_nurture_religion_assimilation, 0.0) \
 	LUA_DEFINES_LIST_ELEMENT(alice_surrender_on_cores_lost, 0.0) \
 	LUA_DEFINES_LIST_ELEMENT(alice_artificial_gp_limitant, 0.0) \
- 	LUA_DEFINES_LIST_ELEMENT(alice_rename_dont_use_localisation, 0.0) \
- 	LUA_DEFINES_LIST_ELEMENT(alice_spherelings_only_ally_sphere, 0.0) \
+	LUA_DEFINES_LIST_ELEMENT(alice_rename_dont_use_localisation, 0.0) \
+	LUA_DEFINES_LIST_ELEMENT(alice_spherelings_only_ally_sphere, 0.0) \
 	LUA_DEFINES_LIST_ELEMENT(alice_overseas_mil, 0.035) \
 	LUA_DEFINES_LIST_ELEMENT(alice_militancy_decay, 0.015) \
 	LUA_DEFINES_LIST_ELEMENT(alice_rgo_per_size_employment, 40000.0) \
