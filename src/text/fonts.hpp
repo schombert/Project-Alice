dfe7af7991a13defe1ab38e9a6909bd7ff744ef3
lizzie <lizzie@eden-emu.dev>
2026-07-21 10:17:08 +0000

    insert_or_find_glyph() instead of sep. get_glyph/make_glyph

--- src/text/fonts.hpp
+++ src/text/fonts.hpp
@@ -209,8 +209,7 @@ public:
 	
 	std::vector<uint32_t> textures;
 
-	void make_glyph(uint16_t glyph_in, int32_t subpixel);
-	glyph_sub_offset& get_glyph(uint16_t glyph_in, int32_t subpixel);
+	std::optional<std::reference_wrapper<glyph_sub_offset>> insert_or_find_glyph(uint16_t glyph_in, int32_t subpixel);
 	void reset();
 	void create(FT_Library lib, FT_Byte* file_data, size_t file_size, int32_t real_size);
 	void remake_cache(sys::state& state, font_selection type, stored_glyphs& txt, std::span<uint16_t> source, uint32_t details_offset = 0, layout_details* d = nullptr, uint16_t font_handle = 0);
